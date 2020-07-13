7f65ed79f0f5aee4740e74de9700cc27c5c46463
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-07-13 15:30:15 +0100

    Added shatter setup.

--- src/bin/render.rs
+++ src/bin/render.rs
@@ -10,27 +10,29 @@ use std::{
 /// Input parameters.
 #[input]
 struct Parameters {
-    // /// Adaptive mesh settings.
-// tree: tree::Settings,
-// /// Regular grid settings.
-// grid: grid::Settings,
-// /// Render runtime settings.
-// sett: render::Settings,
-// /// Surfaces map.
-// surfs: Set<form::Mesh>,
-// /// Colour map.
-// cols: Set<form::Gradient>,
-// /// Attributes map.
-// attrs: Set<render::Attributes>,
-// /// Scenes.
-// scenes: Set<form::Scene>,
+    /// Adaptive mesh settings.
+    tree: tree::Settings,
+    /// Regular grid settings.
+    grid: grid::Settings,
+    // /// Render runtime settings.
+    // sett: render::Settings,
+    /// Surfaces map.
+    surfs: Set<form::Mesh>,
+    // /// Colour map.
+    // cols: Set<form::Gradient>,
+    // /// Attributes map.
+    // attrs: Set<render::Attributes>,
+    // /// Scenes.
+    // scenes: Set<form::Scene>,
 }
 
 /// Main function.
 pub fn main() {
     banner::title("RENDER");
     let (params_path, in_dir, _out_dir) = init();
-    let _params = input(&in_dir, &params_path);
+    let params = input(&in_dir, &params_path);
+    let (tree_sett, grid_sett, surfs) = build(&in_dir, params);
+    let (tree, grid) = grow(tree_sett, grid_sett, &surfs);
     banner::section("Finished");
 }
 
@@ -65,4 +67,75 @@ fn input(in_dir: &Path, params_path: &Path) -> Parameters {
     let path = in_dir.join(params_path);
 
     Parameters::load(&path).expect("Could not load parameters file")
+}
+
+/// Build instances.
+fn build(in_dir: &Path, params: Parameters) -> (tree::Settings, grid::Settings, Set<Mesh>) {
+    banner::section("Building");
+    banner::sub_section("Adaptive Tree Settings");
+    let tree_sett = params.tree;
+    report!("Tree settings", &tree_sett);
+
+    banner::sub_section("Grid Settings");
+    let grid_sett = params.grid;
+    report!("Grid settings", &grid_sett);
+
+    // banner::sub_section("Render Settings");
+    // let render_sett = params.sett;
+    // report!("Render settings", &render_sett);
+
+    banner::sub_section("Surfaces");
+    let surfs = params
+        .surfs
+        .build(in_dir)
+        .expect("Unable to build surfaces.");
+    report!("Surfaces", &surfs);
+
+    // banner::sub_section("Colours");
+    // let cols = params
+    //     .cols
+    //     .build(in_dir)
+    //     .expect("Unable to build colour gradients.");
+    // for (group, grad) in cols.map() {
+    //     report!(&format!("[{}]", group), gradient::to_string(&grad, 32));
+    // }
+
+    // banner::sub_section("Attributes");
+    // let attrs = params.attrs;
+    // report!("Attributes", &attrs);
+
+    // banner::sub_section("Scenes");
+    // let scenes = params
+    //     .scenes
+    //     .build(in_dir)
+    //     .expect("Unable to build scenes.");
+    // report!("Scenes", &scenes);
+
+    (
+        tree_sett, grid_sett,
+        // render_sett,
+        surfs,
+        // cols,
+        // attrs,
+        // scenes,
+    )
+}
+
+/// Grow domains.
+fn grow<'a>(
+    tree_sett: tree::Settings,
+    grid_sett: grid::Settings,
+    surfs: &'a Set<Mesh>,
+) -> (tree::Cell<'a>, grid::Grid) {
+    banner::section("Growing");
+
+    banner::sub_section("Adaptive Tree");
+    let tree = tree::Cell::new_root(&tree_sett, &surfs);
+    report!("Adaptive tree", &tree);
+
+    banner::sub_section("Regular Grid");
+    let grid = grid::Grid::new(&grid_sett);
+    report!("Regular grid", &grid);
+
+    (tree, grid)
 }
