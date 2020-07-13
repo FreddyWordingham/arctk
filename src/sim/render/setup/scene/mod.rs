4a3983a16e0c4e39a0089df255955a0ecf25e3c4
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-07-13 18:14:36 +0100

    Corrected scene strcuture name.

--- src/sim/render/setup/scene/mod.rs
+++ src/sim/render/setup/scene/mod.rs
@@ -8,21 +8,21 @@ use crate::{access, display_field, display_field_ln, form::Camera};
 use attr::load;
 use std::fmt::{Display, Formatter, Result};
 
-/// Loadable technical settings structure.
+/// Scene settings structure.
 #[load]
-pub struct Technical {
+pub struct Scene {
     /// Engine selection.
     cam: Camera,
     /// Lighting properties.
     lighting: Lighting,
 }
 
-impl Technical {
+impl Scene {
     access!(cam, Camera);
     access!(lighting, Lighting);
 }
 
-impl Display for Technical {
+impl Display for Scene {
     #[allow(clippy::result_expect_used)]
     #[inline]
     fn fmt(&self, fmt: &mut Formatter) -> Result {
