60d68b1b4d75de41d33afe627ef180cd34e4fbce
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-07-15 15:41:48 +0100

    Added illumination light function.

--- src/sim/render/engine/test/illumination.rs
+++ src/sim/render/engine/test/illumination.rs
@@ -1,29 +1,25 @@
 //! Illumination functions.
 
-// use crate::{render::Scene, Crossing, Dir3, Hit, Ray};
+use crate::{render::Scene, Crossing, Dir3, Hit, Ray};
 
-// /// Calculate the lighting factor.
-// #[inline]
-// #[must_use]
-// pub fn light(scene: &Scene, ray: &Ray, hit: &Hit) -> f64 {
-//     let light_dir = Dir3::new_normalize(scene.light().sun_pos() - ray.pos());
-//     let view_dir = Dir3::new_normalize(scene.cam().focus().orient().pos() - ray.pos());
-//     let ref_dir = Crossing::init_ref_dir(
-//         ray.dir(),
-//         hit.side().norm(),
-//         -ray.dir().dot(hit.side().norm()),
-//     );
+/// Calculate the lighting factor.
+#[inline]
+#[must_use]
+pub fn light(scene: &Scene, ray: &Ray, hit: &Hit) -> f64 {
+    let light_dir = Dir3::new_normalize(scene.lighting().sky().sun_pos() - ray.pos());
+    let view_dir = Dir3::new_normalize(scene.cam().focus().orient().pos() - ray.pos());
+    let ref_dir = Crossing::calc_ref_dir(ray.dir(), hit.side().norm());
 
-//     let mut ambient = 1.0;
-//     let mut diffuse = hit.side().norm().dot(&light_dir).max(0.0);
-//     let mut specular = view_dir
-//         .dot(&ref_dir)
-//         .max(0.0)
-//         .powi(scene.light().spec_pow());
+    let mut ambient = 1.0;
+    let mut diffuse = hit.side().norm().dot(&light_dir).max(0.0);
+    let mut specular = view_dir
+        .dot(&ref_dir)
+        .max(0.0)
+        .powi(scene.lighting().light().spec_pow());
 
-//     ambient *= scene.light().ambient_light();
-//     diffuse *= scene.light().diffuse_light();
-//     specular *= scene.light().specular_light();
+    ambient *= scene.lighting().light().ambient();
+    diffuse *= scene.lighting().light().diffuse();
+    specular *= scene.lighting().light().specular();
 
-//     ambient + diffuse + specular
-// }
+    ambient + diffuse + specular
+}
