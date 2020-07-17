b044c994e85fa427aa407d23b7f982dc788acecd
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-07-17 16:14:37 +0100

    Added sky noise map.

--- src/file/form/sky.rs
+++ src/file/form/sky.rs
@@ -1,7 +1,8 @@
 //! Sky builder structure.
 
-use crate::{display_field, display_field_ln, Build, Error, Pos3};
+use crate::{display_field, display_field_ln, Build, Error, PerlinMap, Pos3, X, Y};
 use attr::load;
+use rand::thread_rng;
 use std::{
     fmt::{Display, Formatter},
     path::Path,
@@ -16,6 +17,8 @@ pub struct Sky {
     sun_pos: Pos3,
     /// Sun angular radius when calculating soft shadows [deg].
     sun_rad: f64,
+    /// Noise segments.
+    segments: [usize; 2],
 }
 
 impl Build for Sky {
@@ -27,6 +30,7 @@ impl Build for Sky {
             self.brightness,
             self.sun_pos,
             self.sun_rad.to_radians(),
+            PerlinMap::new(self.segments, &mut thread_rng()),
         ))
     }
 }
@@ -37,6 +41,11 @@ impl Display for Sky {
     fn fmt(&self, fmt: &mut Formatter) -> std::fmt::Result {
         display_field_ln!(fmt, "sky brightness", self.brightness)?;
         display_field_ln!(fmt, "sun position", &self.sun_pos, "m")?;
-        display_field!(fmt, "sun radius", self.sun_rad, "deg")
+        display_field_ln!(fmt, "sun radius", self.sun_rad, "deg")?;
+        display_field!(
+            fmt,
+            "segments",
+            &format!("{} x {}", self.segments[X], self.segments[Y])
+        )
     }
 }
--- src/sim/render/setup/scene/lighting/sky.rs
+++ src/sim/render/setup/scene/lighting/sky.rs
@@ -1,6 +1,6 @@
 //! Sky setup structure.
 
-use crate::{access, clone, display_field, display_field_ln, Pos3};
+use crate::{access, clone, display_field, display_field_ln, PerlinMap, Pos3};
 use std::fmt::{Display, Formatter, Result};
 
 /// Lighting structure.
@@ -11,17 +11,20 @@ pub struct Sky {
     sun_pos: Pos3,
     /// Sun angular radius when calculating soft shadows [rad].
     sun_rad: f64,
+    /// Sky perlin noise map.
+    noise: PerlinMap,
 }
 
 impl Sky {
     clone!(brightness, f64);
     access!(sun_pos, Pos3);
     clone!(sun_rad, f64);
+    access!(noise, PerlinMap);
 
     /// Construct a new instance.
     #[inline]
     #[must_use]
-    pub fn new(brightness: f64, sun_pos: Pos3, sun_rad: f64) -> Self {
+    pub fn new(brightness: f64, sun_pos: Pos3, sun_rad: f64, noise: PerlinMap) -> Self {
         debug_assert!(brightness >= 0.0);
         debug_assert!(brightness >= 0.0);
         debug_assert!(sun_rad >= 0.0);
@@ -30,6 +33,7 @@ impl Sky {
             brightness,
             sun_pos,
             sun_rad,
+            noise,
         }
     }
 }
@@ -40,6 +44,7 @@ impl Display for Sky {
     fn fmt(&self, fmt: &mut Formatter) -> Result {
         display_field_ln!(fmt, "sky brightness", self.brightness)?;
         display_field_ln!(fmt, "sun position", &self.sun_pos, "m")?;
-        display_field!(fmt, "sun radius", self.sun_rad.to_degrees(), "deg")
+        display_field_ln!(fmt, "sun radius", self.sun_rad.to_degrees(), "deg")?;
+        display_field!(fmt, "noise", &self.noise)
     }
 }
