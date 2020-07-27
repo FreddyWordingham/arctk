801f697f27c5b9f426007cdeeb4b55b6cf1b1ea8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-07-27 11:55:11 +0100

    Began work on cartographer mapping function.

--- src/sim/cartographer/map.rs
+++ src/sim/cartographer/map.rs
@@ -1,11 +1,18 @@
 //! Mapping function.
 
-use crate::{grid::Grid, Group, Mesh, Set};
+use crate::{grid::Grid, Group, Mesh, Set, X, Y, Z};
 use ndarray::Array3;
 
 /// Map a volume of surfaces.
 #[inline]
 #[must_use]
 pub fn map(grid: &Grid, _surfs: &Set<Mesh>) -> Array3<Group> {
-    Array3::from_shape_vec(*grid.res(), vec![]).unwrap()
+    let res = *grid.res();
+    for _xi in 0..res[X] {
+        for _yi in 0..res[Y] {
+            for _zi in 0..res[Z] {}
+        }
+    }
+
+    Array3::from_shape_vec(res, vec![]).unwrap()
 }
