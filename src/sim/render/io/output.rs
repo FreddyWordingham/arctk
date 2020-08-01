7b6e4227caed553533a3085691e9e168a5500c8d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-08-01 05:08:17 +0100

    Leaving the machine here for now.

--- src/sim/render/io/output.rs
+++ src/sim/render/io/output.rs
@@ -1,9 +1,10 @@
 //! Output structure.
 
-use crate::{X, Y};
-use ndarray::Array2;
-use palette::LinSrgba;
-use std::ops::AddAssign;
+use crate::{Error, X, Y};
+use ndarray::{Array2, ShapeBuilder};
+use palette::{LinSrgba, Pixel, Srgba};
+use png::{BitDepth, ColorType, Encoder};
+use std::{fs::File, io::BufWriter, ops::AddAssign, path::Path};
 
 /// Rendering output structure.
 pub struct Output {
@@ -22,6 +23,32 @@ impl Output {
         Self {
             img: Array2::default(res),
         }
+    }
+
+    /// Save an array as a png image.
+    #[allow(clippy::use_self)]
+    #[inline]
+    fn save_png(img: &Array2<LinSrgba>, path: &Path) -> Result<(), Error> {
+        let res = (img.shape()[0], img.shape()[1]);
+        let mut data = Array2::from_elem((res.0, res.1).f(), [0; 4]);
+        for xi in 0..res.0 {
+            for yi in 0..res.1 {
+                let col = img[(xi, yi)];
+                data[(xi, res.1 - yi - 1)] = Srgba::from_linear(col).into_format().into_raw();
+            }
+        }
+
+        let file = File::create(path)?;
+        let w = BufWriter::new(file);
+
+        let mut encoder = Encoder::new(w, res.0 as u32, res.1 as u32);
+        encoder.set_color(ColorType::RGBA);
+        encoder.set_depth(BitDepth::Eight);
+        let mut writer = encoder.write_header()?;
+
+        writer.write_image_data(data.into_raw_vec().flatten())?;
+
+        Ok(())
     }
 }
 
