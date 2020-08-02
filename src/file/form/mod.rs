e7ef55b7b69d98684ec73b833cfaec771d05d961
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-08-02 20:41:50 +0100

    Moved formula form to formula_builder class.

--- src/file/form/mod.rs
+++ src/file/form/mod.rs
@@ -1,8 +1,7 @@
 //! Form module.
 
-pub mod formula;
 pub mod gradient;
 pub mod mesh;
 pub mod trans3;
 
-pub use self::{formula::*, gradient::*, mesh::*, trans3::*};
+pub use self::{gradient::*, mesh::*, trans3::*};
--- src/sci/math/func/mod.rs
+++ src/sci/math/func/mod.rs
@@ -1,5 +1,7 @@
 //! Mathematical function module.
 
 pub mod formula;
+pub mod formula_builder;
 
 pub use self::formula::*;
+pub use self::formula_builder::*;
