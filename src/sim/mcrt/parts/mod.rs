3481f97e7928c16bd2ddcfb2b9b5367bd2fafb17
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-08-06 13:45:19 +0100

    Added mcrt attributes structure.

--- src/sim/mcrt/parts/mod.rs
+++ src/sim/mcrt/parts/mod.rs
@@ -1,9 +1,10 @@
 //! Rendering simulation parts sub-module.
 
+pub mod attributes;
 pub mod light;
 pub mod light_builder;
 pub mod local;
 pub mod material;
 pub mod photon;
 
-pub use self::{light::*, light_builder::*, local::*, material::*, photon::*};
+pub use self::{attributes::*, light::*, light_builder::*, local::*, material::*, photon::*};
