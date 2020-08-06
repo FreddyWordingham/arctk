d33d182ced0ffd4ff7259b7c453f0c2f51f61edb
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-08-06 14:08:00 +0100

    Added mcrt light-builder structure.

---
+++ src/sim/mcrt/parts/light.rs
@@ -0,0 +1,46 @@
+//! Light surface structure.
+
+use crate::{access, display_field, display_field_ln, mcrt::Photon, Emit, Mesh, Probability};
+use rand::rngs::ThreadRng;
+use std::fmt::{Display, Formatter, Result};
+
+/// Photon emission structure.
+pub struct Light {
+    /// Emission surface.
+    surf: Mesh,
+    /// Emission spectrum.
+    spec: Probability,
+}
+
+impl Light {
+    access!(surf, Mesh);
+    access!(spec, Probability);
+
+    /// Construct a new instance.
+    #[inline]
+    #[must_use]
+    pub const fn new(surf: Mesh, spec: Probability) -> Self {
+        Self { surf, spec }
+    }
+
+    /// Emit a new photon.
+    #[inline]
+    #[must_use]
+    pub fn emit(&self, mut rng: &mut ThreadRng, power: f64) -> Photon {
+        debug_assert!(power > 0.0);
+
+        let ray = self.surf.cast(&mut rng);
+        let wavelength = self.spec.sample(&mut rng);
+
+        Photon::new(ray, wavelength, power)
+    }
+}
+
+impl Display for Light {
+    #[allow(clippy::result_expect_used)]
+    #[inline]
+    fn fmt(&self, fmt: &mut Formatter) -> Result {
+        display_field_ln!(fmt, "surface", &self.surf)?;
+        display_field!(fmt, "spectrum", &self.spec)
+    }
+}
