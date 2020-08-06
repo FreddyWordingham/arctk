6ba0a01ac942de61de19a9fc3588537ca65d47f5
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-08-06 14:18:46 +0100

    Simplified mcrt input settings.

--- src/sim/mcrt/input/settings.rs
+++ src/sim/mcrt/input/settings.rs
@@ -1,6 +1,6 @@
 //! Settings implementation.
 
-use crate::{access, clone, display_field, display_field_ln, Group, Range};
+use crate::{clone, display_field, display_field_ln};
 use attr::load;
 use std::fmt::{Display, Formatter, Result};
 
@@ -15,14 +15,6 @@ pub struct Settings {
     bump_dist: f64,
     /// Loop limit.
     loop_limit: u64,
-    /// Weight to perform roulette at.
-    roulette_weight: f64,
-    /// Number of roulette barrels.
-    roulette_barrels: u64,
-    /// Initial material.
-    init_mat: Group,
-    /// Wavelength limits.
-    range: Range,
 }
 
 impl Settings {
@@ -30,10 +22,6 @@ impl Settings {
     clone!(num_phot, u64);
     clone!(bump_dist, f64);
     clone!(loop_limit, u64);
-    clone!(roulette_weight, f64);
-    clone!(roulette_barrels, u64);
-    access!(init_mat, Group);
-    access!(range, Range);
 }
 
 impl Display for Settings {
@@ -43,10 +31,6 @@ impl Display for Settings {
         display_field_ln!(fmt, "block size", self.block_size)?;
         display_field_ln!(fmt, "number of photons", self.num_phot)?;
         display_field_ln!(fmt, "bump distance", self.bump_dist, "m")?;
-        display_field_ln!(fmt, "loop limit", self.loop_limit)?;
-        display_field_ln!(fmt, "roulette weight", self.roulette_weight)?;
-        display_field_ln!(fmt, "roulette barrels", self.roulette_barrels)?;
-        display_field_ln!(fmt, "initial material", &self.init_mat)?;
-        display_field!(fmt, "wavelength range", &self.range, "m")
+        display_field!(fmt, "loop limit", self.loop_limit)
     }
 }
