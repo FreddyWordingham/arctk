7dea9c0123d4e589bfe0bc266cdc2559fa8cf02f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-08-07 10:04:36 +0100

    Changed mcrt output mode to overwrite.

--- src/sim/mcrt/output/data.rs
+++ src/sim/mcrt/output/data.rs
@@ -64,12 +64,13 @@ impl Display for Data {
 impl Save for Data {
     #[inline]
     fn save(&self, out_dir: &Path) -> Result<(), Error> {
-        // Get current time string.
-        let time = chrono::offset::Local::now()
-            .format("%Y%m%d%H%M%S")
-            .to_string();
-        let path = out_dir.join(time);
-        std::fs::create_dir(&path)?;
+        // // Get current time string.
+        // let time = chrono::offset::Local::now()
+        //     .format("%Y%m%d%H%M%S")
+        //     .to_string();
+        // let path = out_dir.join(time);
+        // std::fs::create_dir(&path)?;
+        let path = out_dir;
 
         let p = path.join("emission_power_density.nc");
         println!("Saving: {}", p.display());
