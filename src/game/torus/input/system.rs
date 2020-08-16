9f28d78ca94097cd03ca5f9a398073b99e8205af
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-08-16 18:20:18 +0100

    Added fps and resolution settings.

--- src/game/torus/input/system.rs
+++ src/game/torus/input/system.rs
@@ -1,23 +1,27 @@
 //! Rendering simulation structure.
 
-use crate::{clone, display_field, X, Y};
+use crate::{clone, display_field, display_field_ln, X, Y};
 use attr::load;
 use std::fmt::{Display, Formatter, Result};
 
 /// Loadable render settings structure.
 #[load]
 pub struct System {
+    /// Frames-per-second limit.
+    fps: i32,
     /// Screen resolution.
     resolution: [i32; 2],
 }
 
 impl System {
+    clone!(fps, i32);
     clone!(resolution, [i32; 2]);
 }
 
 impl Display for System {
     #[inline]
     fn fmt(&self, fmt: &mut Formatter) -> Result {
+        display_field_ln!(fmt, "fps", self.fps)?;
         display_field!(
             fmt,
             "resolution",
