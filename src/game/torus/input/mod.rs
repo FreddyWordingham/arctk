3d2ce22ab5dea9b40271376cd888f98e0050d04a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-08-16 18:56:07 +0100

    Added input structure.

--- src/game/torus/input/mod.rs
+++ src/game/torus/input/mod.rs
@@ -5,3 +5,20 @@ pub mod world;
 
 pub use self::system::*;
 pub use self::world::*;
+
+/// Torus game input structure.
+pub struct Input<'a> {
+    /// System settings.
+    pub sys: &'a System,
+    /// World settings.
+    pub world: &'a World,
+}
+
+impl<'a> Input<'a> {
+    /// Construct a new instance.
+    #[inline]
+    #[must_use]
+    pub const fn new(sys: &'a System, world: &'a World) -> Self {
+        Self { sys, world }
+    }
+}
