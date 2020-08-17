68e00e17505341c634b2547f441d9d46754b18c1
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-08-17 18:47:39 +0100

    Added initial concentration loading.

--- src/sim/kinetics/input/settings.rs
+++ src/sim/kinetics/input/settings.rs
@@ -1,6 +1,6 @@
 //! Reaction simulation structure.
 
-use crate::{clone, display_field};
+use crate::{access, clone, display_field_ln, kinetics::ConcBuilder};
 use attr::load;
 use std::fmt::{Display, Formatter, Result};
 
@@ -9,15 +9,20 @@ use std::fmt::{Display, Formatter, Result};
 pub struct Settings {
     /// Minimum time-step.
     min_time_step: f64,
+    /// Initial concentrations.
+    concs: ConcBuilder,
 }
 
 impl Settings {
     clone!(min_time_step, f64);
+    access!(concs, ConcBuilder);
 }
 
 impl Display for Settings {
     #[inline]
     fn fmt(&self, fmt: &mut Formatter) -> Result {
-        display_field!(fmt, "minimum time step", self.min_time_step, "s")
+        display_field_ln!(fmt, "minimum time step", self.min_time_step, "s")?;
+        // display_field!(fmt, "initial concentrations", self.concs)
+        Ok(())
     }
 }
