6b52e45f195a1144eafa7c81e100d940030fe203
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-08-17 19:03:55 +0100

    Added reaction rate builder.

--- src/sim/kinetics/parts/mod.rs
+++ src/sim/kinetics/parts/mod.rs
@@ -5,6 +5,7 @@ pub mod name;
 pub mod rate;
 pub mod rate_builder;
 pub mod reaction;
+pub mod reaction_builder;
 pub mod register;
 
 pub use self::conc_builder::*;
@@ -12,4 +13,5 @@ pub use self::name::*;
 pub use self::rate::*;
 pub use self::rate_builder::*;
 pub use self::reaction::*;
+pub use self::reaction_builder::*;
 pub use self::register::*;
---
+++ src/sim/kinetics/parts/reaction_builder.rs
@@ -0,0 +1,37 @@
+//! Reaction builder implementation.
+
+use crate::{
+    kinetics::{Name, RateBuilder, Reaction, Register},
+    Error, Group,
+};
+use attr::load;
+
+/// Loadable reaction structure.
+#[load]
+pub struct ReactionBuilder {
+    /// Reaction reactants and their respective stoichiometric coefficients.
+    reactants: Vec<(i32, Group)>,
+    /// Reaction products and their respective stoichiometric coefficients.
+    products: Vec<(i32, Group)>,
+    /// Reaction rate.
+    rate: RateBuilder,
+}
+
+impl Name for ReactionBuilder {
+    type Inst = Reaction;
+
+    #[inline]
+    fn register(self, reg: &Register) -> Result<Self::Inst, Error> {
+        let mut reacts = Vec::with_capacity(self.reactants.len());
+        for (s, name) in self.reactants {
+            reacts.push((s, reg.index(&name)));
+        }
+
+        let mut prods = Vec::with_capacity(self.products.len());
+        for (s, name) in self.products {
+            prods.push((s, reg.index(&name)));
+        }
+
+        Ok(Self::Inst::new(reacts, prods, self.rate.register(reg)?))
+    }
+}
