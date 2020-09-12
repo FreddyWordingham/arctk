eaf5537567a9be3da7b6c33118fe965d60c518ae
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-09-12 23:57:31 +0100

    Added progress bars to tools.

--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -1,6 +1,8 @@
 //! Useful tools.
 
+pub mod bar;
 pub mod binner;
 pub mod range;
+pub mod silent_bar;
 
-pub use self::{binner::*, range::*};
+pub use self::{bar::*, binner::*, range::*, silent_bar::*};
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -4,6 +4,4 @@ pub mod fmt;
 pub mod install;
 pub mod list;
 
-pub use self::fmt::*;
-pub use self::install::*;
-pub use self::list::*;
+pub use self::{fmt::*, install::*, list::*};
