8535f97d7e70954c5dd1a6391205fb2730580fb1
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-10-30 22:21:13 +0000

    Added reaction structure.

--- src/chem/mod.rs
+++ src/chem/mod.rs
@@ -5,8 +5,9 @@
 // pub mod flask_builder;
 pub mod rate;
 // pub mod rate_builder;
-// pub mod reaction;
+pub mod reaction;
 // pub mod reaction_builder;
 // pub mod reactor;
 
 pub use self::rate::*;
+pub use self::reaction::*;
---
+++ src/chem/reaction.rs
@@ -0,0 +1,41 @@
+//! Reaction structure.
+
+use crate::{access, chem::Rate};
+use ndarray::Array1;
+
+/// Reaction.
+#[derive(Clone)]
+pub struct Reaction {
+    /// Reaction rate.
+    rate: Rate,
+    /// Stoichiometric coefficient map.
+    coeffs: Array1<f64>,
+}
+
+impl Reaction {
+    access!(coeffs, Array1<f64>);
+
+    /// Construct a new instance.
+    #[inline]
+    #[must_use]
+    pub fn new(rate: Rate, coeffs: Array1<f64>) -> Self {
+        debug_assert!(!coeffs.is_empty());
+
+        Self { rate, coeffs }
+    }
+
+    /// Determine the rate of change for each chemical within the system.
+    #[inline]
+    #[must_use]
+    pub fn rate(&self, concs: &Array1<f64>) -> Array1<f64> {
+        self.rate.rate(concs) * &self.coeffs
+    }
+
+    /// Separate into components.
+    #[allow(clippy::missing_const_for_fn)]
+    #[inline]
+    #[must_use]
+    pub fn components(self) -> (Rate, Array1<f64>) {
+        (self.rate, self.coeffs)
+    }
+}
