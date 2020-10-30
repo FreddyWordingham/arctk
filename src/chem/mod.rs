cc0e0ab3910ec74240b8b47d62028c170bd01f0a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-10-30 23:30:12 +0000

    Added rate builder structure.

--- src/chem/mod.rs
+++ src/chem/mod.rs
@@ -4,9 +4,9 @@ pub mod conc_builder;
 // pub mod flask;
 // pub mod flask_builder;
 pub mod rate;
-// pub mod rate_builder;
+pub mod rate_builder;
 pub mod reaction;
 // pub mod reaction_builder;
 pub mod reactor;
 
-pub use self::{conc_builder::*, rate::*, reaction::*, reactor::*};
+pub use self::{conc_builder::*, rate::*, rate_builder::*, reaction::*, reactor::*};
---
+++ src/chem/rate_builder.rs
@@ -0,0 +1,40 @@
+//! Rate building structure.
+
+use crate::chem::Rate;
+use crate::err::Error;
+use crate::ord::{Name, Register};
+
+/// Rate of reaction builder.
+#[derive(Clone)]
+pub struct RateBuilder(f64, Vec<(String, f64)>);
+
+impl RateBuilder {
+    /// Get a list of all species involved in the rate calculation.
+    #[inline]
+    #[must_use]
+    pub fn names(&self) -> Vec<String> {
+        let mut names = Vec::new();
+        for (n, _) in &self.1 {
+            names.push(n.to_string());
+        }
+
+        names.sort();
+        names.dedup();
+
+        names
+    }
+}
+
+impl Name for RateBuilder {
+    type Inst = Rate;
+
+    #[inline]
+    fn reg(self, reg: &Register) -> Result<Self::Inst, Error> {
+        let mut orders = Vec::with_capacity(self.1.len());
+        for (name, m) in self.1 {
+            orders.push((reg.index(&name), m))
+        }
+
+        Ok(Self::Inst::new(self.0, orders))
+    }
+}
