b3b57c95cdfbaa5de7da39a03ad7fa71f450834e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-10-30 22:58:22 +0000

    Added register.

--- src/ord/mod.rs
+++ src/ord/mod.rs
@@ -1,7 +1,8 @@
 //! Ordering module.
 
 pub mod list;
+pub mod register;
 pub mod set;
 pub mod sort;
 
-pub use self::{list::*, set::*, sort::*};
+pub use self::{list::*, register::*, set::*, sort::*};
