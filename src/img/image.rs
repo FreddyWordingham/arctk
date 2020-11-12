c6a6f82e58f355bb4d9832aba693dc295efae106
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-11-12 01:57:03 +0000

    Simplified image constructors.

--- src/img/image.rs
+++ src/img/image.rs
@@ -2,12 +2,10 @@
 
 use crate::{
     access,
-    err::Error,
     ord::{X, Y},
 };
 use ndarray::Array2;
-use ndarray_stats::QuantileExt;
-use palette::{Gradient, LinSrgba};
+use palette::LinSrgba;
 use std::ops::AddAssign;
 
 /// Image builder.
@@ -22,42 +20,20 @@ impl Image {
     /// Construct a new instance.
     #[inline]
     #[must_use]
-    pub fn new(res: [usize; 2], base: LinSrgba) -> Self {
+    pub const fn new(pixels: Array2<LinSrgba>) -> Self {
+        Self { pixels }
+    }
+
+    /// Construct a new blank instance.
+    #[inline]
+    #[must_use]
+    pub fn new_blank(res: [usize; 2], base: LinSrgba) -> Self {
         debug_assert!(res[X] > 0);
         debug_assert!(res[Y] > 0);
 
         Self {
             pixels: Array2::from_elem(res, base),
         }
-    }
-
-    /// Construct a new instance from numerical data and a gradient.
-    /// # Errors
-    /// if the minimum or maximum data value can not be determined.
-    #[inline]
-    pub fn data_to_linear(data: &Array2<f64>, grad: &Gradient<LinSrgba>) -> Result<Self, Error> {
-        let min = *data.min()?;
-        let max = *data.max()?;
-        let linear = (data + min) / (max - min);
-
-        Ok(Self {
-            pixels: linear.map(|x| grad.get(*x as f32)),
-        })
-    }
-
-    /// Construct a new instance from numerical data and a gradient.
-    /// # Errors
-    /// if the minimum or maximum data value can not be determined.
-    #[inline]
-    pub fn data_to_log(data: &Array2<f64>, grad: &Gradient<LinSrgba>) -> Result<Self, Error> {
-        let min = *data.min()?;
-        let max = *data.max()?;
-        let linear = (data + min) / (max - min);
-        let log = linear.map(|x| x.log(max));
-
-        Ok(Self {
-            pixels: log.map(|x| grad.get(*x as f32)),
-        })
     }
 }
 
