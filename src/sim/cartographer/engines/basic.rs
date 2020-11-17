a6833e65caf343c3ad87fad1336966c5196a70dc
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-11-17 21:02:32 +0000

    Added super sampling.

--- src/sim/cartographer/engines/basic.rs
+++ src/sim/cartographer/engines/basic.rs
@@ -6,6 +6,13 @@ use rand::rngs::ThreadRng;
 /// Determine what a single ray observes.
 #[allow(clippy::expect_used)]
 #[inline]
-pub fn basic(_input: &Input, mut _rng: &mut ThreadRng, _index: [usize; 3], mut _data: &mut Output) {
-    
+pub fn basic(input: &Input, mut _rng: &mut ThreadRng, _index: [usize; 3], mut _data: &mut Output) {
+    let bump_dist = input.sett.bump_dist();
+
+    let super_samples = input.sett.super_sampling().num_samples();
+    let weight = 1.0 / super_samples as f64;
+
+    let num_casts = input.sett.caster().num_casts();
+
+    for n in 0..super_samples {}
 }
--- src/sim/cartographer/parts/settings.rs
+++ src/sim/cartographer/parts/settings.rs
@@ -1,6 +1,6 @@
 //! Cartographer settings.
 
-use crate::clone;
+use crate::{access, clone, sim::cartographer::SuperSample};
 use arctk_attr::load;
 
 /// General settings structure.
@@ -10,22 +10,26 @@ pub struct Settings {
     block_size: usize,
     /// Bump distance [m].
     bump_dist: f64,
+    /// Super sampling.
+    super_sampling: SuperSample,
 }
 
 impl Settings {
     clone!(block_size, usize);
     clone!(bump_dist, f64);
+    access!(super_sampling, SuperSample);
 
     /// Construct a new instance.
     #[inline]
     #[must_use]
-    pub fn new(block_size: usize, bump_dist: f64) -> Self {
+    pub fn new(block_size: usize, bump_dist: f64, super_sampling: SuperSample) -> Self {
         debug_assert!(block_size > 0);
         debug_assert!(bump_dist > 0.0);
 
         Self {
             block_size,
             bump_dist,
+            super_sampling,
         }
     }
 }
