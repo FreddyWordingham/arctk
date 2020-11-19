01344a4ec25ca0312b0ff13335da8ea73c6c047c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-11-19 16:45:49 +0000

    Removed distance tracking.

--- src/sim/render/pipe/output.rs
+++ src/sim/render/pipe/output.rs
@@ -14,8 +14,6 @@ use std::{ops::AddAssign, path::Path};
 
 /// Rendering output data.
 pub struct Output<'a> {
-    /// Flight distances.
-    pub dist: Array2<f64>,
     /// Render time.
     pub time: Array2<f64>,
     /// Final surface normal.
@@ -39,7 +37,6 @@ impl<'a> Output<'a> {
         debug_assert!(res[Y] > 0);
 
         Self {
-            dist: Array2::zeros(res),
             time: Array2::zeros(res),
             final_norm: Array2::default(res),
             light: Array2::zeros(res),
@@ -53,7 +50,6 @@ impl<'a> Output<'a> {
 impl<'a> AddAssign<&Self> for Output<'a> {
     #[inline]
     fn add_assign(&mut self, rhs: &Self) {
-        self.dist += &rhs.dist;
         self.time += &rhs.time;
         self.final_norm += &rhs.final_norm;
         self.light += &rhs.light;
@@ -65,11 +61,6 @@ impl<'a> AddAssign<&Self> for Output<'a> {
 impl<'a> Save for Output<'a> {
     #[inline]
     fn save_data(&self, out_dir: &Path) -> Result<(), Error> {
-        let max_dist = self.dist.max()?;
-        report!("Maximum distance", max_dist, "m");
-        Image::new(self.dist.map(|x| self.grad.get((*x / max_dist) as f32)))
-            .save(&out_dir.join("distance.png"))?;
-
         let max_time = self.time.max()?;
         report!("Maximum time", max_time, "ms");
         Image::new(self.time.map(|x| self.grad.get((*x / max_time) as f32)))
