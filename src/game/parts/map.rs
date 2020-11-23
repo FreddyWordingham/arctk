b7411db86589c0f91a3f59fab86e52fb7ac7b163
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-11-23 17:19:25 +0000

    Added map size getters.

--- src/game/parts/map.rs
+++ src/game/parts/map.rs
@@ -3,7 +3,6 @@
 use crate::{
     game::Tile,
     ord::{X, Y},
-    tools::two_dim_to_linear,
 };
 use ndarray::Array2;
 
@@ -20,7 +19,7 @@ impl Map {
     pub fn new(res: [usize; 2]) -> Self {
         let mut tiles = Array2::default(res);
 
-        let [widht, height] = res;
+        let [width, height] = res;
 
         // Boundaries walls
         for x in 0..res[X] {
@@ -29,9 +28,23 @@ impl Map {
         }
         for y in 0..res[Y] {
             tiles[[0, y]] = Tile::Wall;
-            tiles[[widht - 1, y]] = Tile::Wall;
+            tiles[[width - 1, y]] = Tile::Wall;
         }
 
         Self { tiles }
+    }
+
+    /// Get the map height.
+    #[inline]
+    #[must_use]
+    pub fn height(&self) -> usize {
+        self.tiles.nrows()
+    }
+
+    /// Get the map width.
+    #[inline]
+    #[must_use]
+    pub fn width(&self) -> usize {
+        self.tiles.ncols()
     }
 }
