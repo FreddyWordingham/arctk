55b5e82ac4a56f007a093258aa9e4cd45eba5b86
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-11-26 14:25:33 +0000

    Added range field to viewshed component.

--- src/game/comp/viewshed.rs
+++ src/game/comp/viewshed.rs
@@ -5,13 +5,18 @@ use specs_derive::Component;
 
 /// Field of view.
 #[derive(Component)]
-pub struct Viewshed {}
+pub struct Viewshed {
+    /// Maximum view range.
+    pub range: i32,
+}
 
 impl Viewshed {
     /// Construct a new instance.
     #[inline]
     #[must_use]
-    pub const fn new() -> Self {
-        Self {}
+    pub const fn new(range: i32) -> Self {
+        debug_assert!(range > 0);
+
+        Self { range }
     }
 }
