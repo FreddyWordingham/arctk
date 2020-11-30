ed6abd7feb3966d2026fad45a65d2a47ef7000ed
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-11-30 00:56:11 +0000

    Added tile reveal.

--- src/game/sys/visibility.rs
+++ src/game/sys/visibility.rs
@@ -1,8 +1,8 @@
 //! Visibility system
 
-use crate::game::{Map, Position, Viewshed};
+use crate::game::{Map, Player, Position, Viewshed};
 use rltk::{field_of_view, Point};
-use specs::{Join, ReadExpect, System, WriteStorage};
+use specs::{Entities, Join, ReadStorage, System, WriteExpect, WriteStorage};
 
 /// Visibility system.
 pub struct Visibility {}
@@ -18,23 +18,31 @@ impl Visibility {
 
 impl<'a> System<'a> for Visibility {
     type SystemData = (
-        ReadExpect<'a, Map>,
+        WriteExpect<'a, Map>,
+        Entities<'a>,
         WriteStorage<'a, Viewshed>,
         WriteStorage<'a, Position>,
+        ReadStorage<'a, Player>,
     );
 
     #[inline]
-    fn run(&mut self, (map, mut viewshed, mut pos): Self::SystemData) {
+    fn run(&mut self, (mut map, entities, mut viewshed, mut pos, player): Self::SystemData) {
         let w = map.width() as i32;
         let h = map.height() as i32;
 
-        for (viewshed, pos) in (&mut viewshed, &mut pos).join() {
+        for (ent, viewshed, pos) in (&entities, &mut viewshed, &mut pos).join() {
             viewshed.visible_tiles.clear();
             viewshed.visible_tiles =
                 field_of_view(Point::new(pos.p.x, pos.p.y), viewshed.range, &*map);
             viewshed
                 .visible_tiles
                 .retain(|p| p.x >= 0 && p.x < w && p.y >= 0 && p.y < h);
+
+            if player.get(ent).is_some() {
+                for vis in &viewshed.visible_tiles {
+                    map.revealed[[vis.x as usize, vis.y as usize]] = true;
+                }
+            }
         }
     }
 }
