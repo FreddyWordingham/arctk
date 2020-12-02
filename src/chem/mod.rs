7e22eb19380509adb9d0550924f668690476f9b5
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-12-02 16:45:00 +0000

    Implmentented link for concentrations set.

--- src/chem/mod.rs
+++ src/chem/mod.rs
@@ -1,11 +1,11 @@
 //! Chemical kinetics module.
 
-pub mod conc_linker;
+pub mod concentrations;
 // pub mod rate;
 // pub mod rate_builder;
 // pub mod reaction;
 // pub mod reaction_builder;
 // pub mod reactor;
 
-pub use self::conc_linker::*;
+pub use self::concentrations::*;
 // conc_builder::*, rate::*, rate_builder::*, reaction::*, reaction_builder::*, reactor::*,
