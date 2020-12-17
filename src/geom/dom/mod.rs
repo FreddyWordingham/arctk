5e91dcfa8838deb22e6fd3ed84e4ef89f73f986e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-12-17 21:10:17 +0000

    Removed overly specialised intermediate structures.

--- src/geom/dom/mod.rs
+++ src/geom/dom/mod.rs
@@ -3,12 +3,7 @@
 pub mod grid;
 pub mod grid_builder;
 pub mod surface;
-pub mod surface_builder;
-pub mod surface_linker;
 pub mod tree;
 pub mod tree_settings;
 
-pub use self::{
-    grid::*, grid_builder::*, surface::*, surface_builder::*, surface_linker::*, tree::*,
-    tree_settings::*,
-};
+pub use self::{grid::*, grid_builder::*, surface::*, tree::*, tree_settings::*};
