b8f10ea5264e1dc442692a55d6f39bb5d69f7031
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-12-17 21:23:13 +0000

    Added babbage initialisation.

--- src/bin/babbage.rs
+++ src/bin/babbage.rs
@@ -4,9 +4,10 @@
 use arctk::{
     args,
     file::{Build, Load},
+    report,
     // sim::babbage::OperationBuilder,
     util::{
-        banner::{section, title},
+        banner::{section, sub_section, title},
         dir,
     },
 };
@@ -20,4 +21,35 @@ use std::{env::current_dir, path::PathBuf};
 //     op: OperationBuilder,
 // }
 
-fn main() {}
+fn main() {
+    let term_width = arctk::util::term::width().unwrap_or(80);
+    title(term_width, "Babbage");
+
+    let (_in_dir, _out_dir) = initialisation(term_width);
+
+    section(term_width, "Finished");
+}
+
+fn initialisation(term_width: usize) -> (PathBuf, PathBuf) {
+    section(term_width, "Initialisation");
+    sub_section(term_width, "args");
+    args!(
+        bin_path: PathBuf;
+        input_dir: PathBuf;
+        output_dir: PathBuf;
+        params_path: PathBuf
+    );
+    report!(bin_path.display(), "binary path");
+    report!(input_dir.display(), "relative input path");
+    report!(output_dir.display(), "relative output path");
+    report!(params_path.display(), "parameters");
+
+    sub_section(term_width, "directories");
+    let cwd = current_dir().expect("Failed to determine current working directory.");
+    let (in_dir, out_dir) = dir::io_dirs(Some(cwd.join(input_dir)), Some(cwd.join(output_dir)))
+        .expect("Failed to initialise directories.");
+    report!(in_dir.display(), "input directory");
+    report!(out_dir.display(), "output directory");
+
+    (in_dir, out_dir)
+}
