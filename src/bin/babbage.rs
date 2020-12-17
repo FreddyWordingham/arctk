7c0766240358ff2b3c68c81b2449a90a27b7c2de
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-12-17 21:03:54 +0000

    Added babbage binary.

---
+++ src/bin/babbage.rs
@@ -0,0 +1,23 @@
+//! Datacube manipulation engine binary.
+//! Produce some data from some other data.
+
+use arctk::{
+    args,
+    file::{Build, Load},
+    // sim::babbage::OperationBuilder,
+    util::{
+        banner::{section, title},
+        dir,
+    },
+};
+use arctk_attr::input;
+use std::{env::current_dir, path::PathBuf};
+
+// // Input parameters.
+// #[input]
+// struct Parameters {
+//     /// Operation to perform.
+//     op: OperationBuilder,
+// }
+
+fn main() {}
