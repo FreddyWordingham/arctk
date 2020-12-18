51dd481432bc08a8834ce2042732d8bd8d85b3eb
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-12-18 20:57:59 +0000

    Added input settings files.

--- src/sim/cartographer/parameters.rs
+++ src/sim/cartographer/parameters.rs
@@ -1,12 +1,24 @@
 //! Input parameters.
 
 // use crate::fmt_report;
+use crate::{
+    file::Redirect,
+    geom::{GridBuilder, TreeSettings},
+    sim::cartographer::Settings,
+};
 use arctk_attr::load;
 use std::fmt::{Display, Error, Formatter};
 
 /// Runtime parameters.
 #[load]
-pub struct Parameters {}
+pub struct Parameters {
+    /// Simulation specific settings.
+    sett: Redirect<Settings>,
+    /// Tree settings.
+    tree: Redirect<TreeSettings>,
+    /// Measurement grid settings.
+    grid: Redirect<GridBuilder>,
+}
 
 impl Display for Parameters {
     #[inline]
