f71d2fd59c7b34f75c1d347e50eb1af408ac8550
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-12-18 21:00:30 +0000

    Added input parameters reporting.

--- src/sim/cartographer/parameters.rs
+++ src/sim/cartographer/parameters.rs
@@ -1,6 +1,6 @@
 //! Input parameters.
 
-// use crate::fmt_report;
+use crate::fmt_report;
 use crate::{
     file::Redirect,
     geom::{GridBuilder, TreeSettings},
@@ -24,6 +24,9 @@ impl Display for Parameters {
     #[inline]
     fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
         writeln!(fmt, "...")?;
+        fmt_report!(fmt, self.sett, "settings");
+        fmt_report!(fmt, self.tree, "tree settings");
+        fmt_report!(fmt, self.grid, "grid settings");
         Ok(())
     }
 }
