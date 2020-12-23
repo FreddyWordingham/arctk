9e167f383b19f5336469ccff39983c61fa05c53d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-12-23 20:01:06 +0000

    Added tracer structure.

--- src/sim/render/mod.rs
+++ src/sim/render/mod.rs
@@ -7,9 +7,11 @@ pub mod attribute_linker;
 // pub mod parameters;
 // pub mod parameters_builder;
 // pub mod parameters_builder_loader;
-// pub mod run;
 pub mod settings;
 pub mod shader;
 pub mod shader_linker;
+pub mod tracer;
 
-pub use self::{attribute::*, attribute_linker::*, settings::*, shader::*, shader_linker::*};
+pub use self::{
+    attribute::*, attribute_linker::*, settings::*, shader::*, shader_linker::*, tracer::*,
+};
