a5410317f18c55370484746d14e3f7937751523a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-12-23 18:58:06 +0000

    Implemented link trait for attribute linker.

--- src/sim/render/attribute_linker.rs
+++ src/sim/render/attribute_linker.rs
@@ -1,12 +1,13 @@
 //! Visual attributes.
 
 use crate::{
+    err::Error,
     img::Gradient,
     ord::{Link, Name, Set},
     sim::render::Attribute,
 };
 use arctk_attr::file;
-use std::fmt::{Display, Error, Formatter};
+use std::fmt::{Display, Formatter};
 
 /// Surface attribute setup.
 #[file]
@@ -23,9 +24,60 @@ pub enum AttributeLinker {
     Luminous(Name, f64),
 }
 
+impl<'a> Link<'a, Gradient> for AttributeLinker {
+    type Inst = Attribute<'a>;
+
+    #[inline]
+    fn requires(&self) -> Vec<Name> {
+        match *self {
+            Self::Opaque(ref grad)
+            | Self::Mirror(ref grad, ..)
+            | Self::Transparent(ref grad, ..)
+            | Self::Refractive(ref grad, ..)
+            | Self::Luminous(ref grad, ..) => vec![grad.clone()],
+        }
+    }
+
+    #[inline]
+    fn link(self, grads: &'a Set<Gradient>) -> Result<Self::Inst, Error> {
+        Ok(match self {
+            Self::Opaque(ref grad) => Attribute::Opaque(
+                grads
+                    .get(grad)
+                    .unwrap_or_else(|| panic!("Failed to link attribute-gradient key: {}", grad)),
+            ),
+            Self::Mirror(ref grad, abs_frac) => Attribute::Mirror(
+                grads
+                    .get(grad)
+                    .unwrap_or_else(|| panic!("Failed to link attribute-gradient key: {}", grad)),
+                abs_frac,
+            ),
+            Self::Transparent(ref grad, abs_frac) => Attribute::Transparent(
+                grads
+                    .get(grad)
+                    .unwrap_or_else(|| panic!("Failed to link attribute-gradient key: {}", grad)),
+                abs_frac,
+            ),
+            Self::Refractive(ref grad, abs_frac, ref_indices) => Attribute::Refractive(
+                grads
+                    .get(grad)
+                    .unwrap_or_else(|| panic!("Failed to link attribute-gradient key: {}", grad)),
+                abs_frac,
+                ref_indices,
+            ),
+            Self::Luminous(ref grad, bright_mult) => Attribute::Luminous(
+                grads
+                    .get(grad)
+                    .unwrap_or_else(|| panic!("Failed to link attribute-gradient key: {}", grad)),
+                bright_mult,
+            ),
+        })
+    }
+}
+
 impl Display for AttributeLinker {
     #[inline]
-    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), std::fmt::Error> {
         match *self {
             Self::Opaque(ref grad) => {
                 write!(fmt, "Opaque: [{}]", grad)
