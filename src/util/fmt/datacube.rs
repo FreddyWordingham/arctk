e7437406ce0d66d544c2917d9fc63476c4ca5f0f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-12-27 19:27:26 +0000

    Added datasquare formatting function.

--- src/util/fmt/datacube.rs
+++ src/util/fmt/datacube.rs
@@ -4,9 +4,41 @@ use crate::{
     fmt_report,
     ord::{X, Y, Z},
 };
-use ndarray::Array3;
+use ndarray::{Array2, Array3};
 use ndarray_stats::QuantileExt;
 use std::fmt::Formatter;
+
+/// Write a datasquares properties to a given formatter.
+/// # Errors
+/// if the minimum value of the data array can not be determined or
+/// if the maximum value of the data array can not be determined.
+#[inline]
+pub fn display_datasquare(
+    fmt: &mut Formatter,
+    datasquare: &Array2<f64>,
+) -> Result<(), std::fmt::Error> {
+    writeln!(fmt, "...")?;
+    let res = datasquare.shape();
+    fmt_report!(fmt, &format!("[{} x {}]", res[X], res[Y]), "resolution");
+    fmt_report!(
+        fmt,
+        datasquare
+            .min()
+            .unwrap_or_else(|_| panic!("Failed to determine minimum value.")),
+        "minimum"
+    );
+    fmt_report!(
+        fmt,
+        datasquare
+            .max()
+            .unwrap_or_else(|_| panic!("Failed to determine maximum value.")),
+        "maximum"
+    );
+    let sum = datasquare.sum();
+    fmt_report!(fmt, sum / datasquare.len() as f64, "average");
+    fmt_report!(fmt, sum, "sum");
+    Ok(())
+}
 
 /// Write a datacubes properties to a given formatter.
 /// # Errors
