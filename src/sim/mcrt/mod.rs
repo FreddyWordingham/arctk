340e4385f963ea1836af2bf89f7b03882d9180b7
Freddy Wordingham <fjmw201@exeter.ac.uk>
2021-01-04 15:56:39 +0000

    Added mcrt settings structure.

--- src/sim/mcrt/mod.rs
+++ src/sim/mcrt/mod.rs
@@ -1,19 +1,16 @@
 //! Monte Carlo Radiative-transfer module.
 
-pub mod attribute;
-pub mod attribute_linker;
-pub mod engine;
-pub mod engine_builder;
-pub mod engines;
-pub mod input;
-pub mod output;
-pub mod parameters;
-pub mod parameters_builder;
-pub mod parameters_builder_loader;
-pub mod run;
+// pub mod attribute;
+// pub mod attribute_linker;
+// pub mod engine;
+// pub mod engine_builder;
+// pub mod engines;
+// pub mod input;
+// pub mod output;
+// pub mod parameters;
+// pub mod parameters_builder;
+// pub mod parameters_builder_loader;
+// pub mod run;
 pub mod settings;
 
-pub use self::{
-    attribute::*, attribute_linker::*, engine::*, engine_builder::*, input::*, output::*,
-    parameters::*, parameters_builder::*, parameters_builder_loader::*, run::*, settings::*,
-};
+pub use self::settings::*;
---
+++ src/sim/mcrt/settings.rs
@@ -0,0 +1,55 @@
+//! MCRT settings.
+
+use crate::{access, clone};
+
+/// General settings structure.
+pub struct Settings {
+    /// Number of photons to simulate.
+    num_phot: usize,
+    /// Number of photons to simulate in each thread block.
+    block_size: usize,
+    /// Bump distance [m].
+    bump_dist: f64,
+    /// Loop limit.
+    loop_limit: u64,
+    /// Minimum statistical weight to consider.
+    min_weight: f64,
+    /// Number of roulette barrels.
+    roulette_barrels: u64,
+}
+
+impl Settings {
+    clone!(num_phot, usize);
+    clone!(block_size, usize);
+    clone!(bump_dist, f64);
+    clone!(loop_limit, u64);
+    clone!(min_weight, f64);
+    clone!(roulette_barrels, u64);
+
+    /// Construct a new instance.
+    #[inline]
+    #[must_use]
+    pub fn new(
+        num_phot: usize,
+        block_size: usize,
+        bump_dist: f64,
+        loop_limit: u64,
+        min_weight: f64,
+        roulette_barrels: u64,
+    ) -> Self {
+        debug_assert!(num_phot > 0);
+        debug_assert!(block_size > 0);
+        debug_assert!(bump_dist > 0.0);
+        debug_assert!(min_weight >= 0.0);
+        debug_assert!(roulette_barrels > 1);
+
+        Self {
+            num_phot,
+            block_size,
+            bump_dist,
+            loop_limit,
+            min_weight,
+            roulette_barrels,
+        }
+    }
+}
