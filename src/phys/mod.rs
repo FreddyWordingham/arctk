d8d677b79c550d4b4c1cc332c3632945193f98b4
Freddy Wordingham <fjmw201@exeter.ac.uk>
2021-01-05 20:49:28 +0000

    Added material builder structure.

--- src/phys/mod.rs
+++ src/phys/mod.rs
@@ -3,6 +3,7 @@
 pub mod crossing;
 pub mod local;
 pub mod material;
+pub mod material_builder;
 pub mod photon;
 
-pub use self::{crossing::*, local::*, material::*, photon::*};
+pub use self::{crossing::*, local::*, material::*, material_builder::*, photon::*};
