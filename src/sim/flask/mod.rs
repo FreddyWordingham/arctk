b2c1b5e532eb2d04586a5eaabdf214d101edb980
Freddy Wordingham <fjmw201@exeter.ac.uk>
2021-01-20 14:47:03 +0000

    Added flask settings structure.

--- src/sim/flask/mod.rs
+++ src/sim/flask/mod.rs
@@ -1,7 +1,10 @@
 //! Flask kinetics module.
 
+pub mod settings;
 // pub mod input;
 // pub mod output;
 // pub mod parameters;
 // pub mod parameters_builder;
 // pub mod parameters_builder_loader;
+
+pub use self::settings::*;
---
+++ src/sim/flask/parameters_builder_loader.rs
@@ -0,0 +1,53 @@
+//! Loadable parameters.
+
+use crate::{
+    err::Error,
+    fs::{Load, Redirect},
+    geom::{GridBuilder, SurfaceLinkerLoader, TreeSettings},
+    ord::Set,
+    phys::{LightLinkerBuilderLoader, MaterialBuilder},
+    sim::mcrt::{AttributeLinker, EngineBuilder, ParametersBuilder, Settings},
+};
+use arctk_attr::file;
+use std::path::Path;
+
+/// Loadable runtime parameters.
+#[file]
+pub struct ParametersBuilderLoader {
+    /// Simulation specific settings.
+    sett: Redirect<Settings>,
+    /// Tree settings.
+    tree: Redirect<TreeSettings>,
+    /// Measurement grid settings.
+    grid: Redirect<GridBuilder>,
+    /// Surfaces.
+    surfs: Redirect<Set<SurfaceLinkerLoader>>,
+    /// Attributes.
+    attrs: Redirect<Set<AttributeLinker>>,
+    /// Materials.
+    mats: Redirect<Set<Redirect<MaterialBuilder>>>,
+    /// Main light.
+    light: Redirect<LightLinkerBuilderLoader>,
+    /// Engine selection.
+    engine: EngineBuilder,
+}
+
+impl Load for ParametersBuilderLoader {
+    type Inst = ParametersBuilder;
+
+    #[inline]
+    fn load(self, in_dir: &Path) -> Result<Self::Inst, Error> {
+        let sett = self.sett.load(in_dir)?;
+        let tree = self.tree.load(in_dir)?;
+        let grid = self.grid.load(in_dir)?;
+        let surfs = self.surfs.load(in_dir)?.load(in_dir)?;
+        let attrs = self.attrs.load(in_dir)?;
+        let mats = self.mats.load(in_dir)?.load(in_dir)?;
+        let light = self.light.load(in_dir)?.load(in_dir)?;
+        let engine = self.engine;
+
+        Ok(Self::Inst::new(
+            sett, tree, grid, surfs, attrs, mats, light, engine,
+        ))
+    }
+}
