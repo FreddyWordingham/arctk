71c868c44a9a88feb546260abbc51b35589c3153
Freddy Wordingham <fjmw201@exeter.ac.uk>
2021-01-23 21:56:56 +0000

    Added diffusion settings.

--- src/sim/diffuse/mod.rs
+++ src/sim/diffuse/mod.rs
@@ -4,10 +4,10 @@
 // pub mod parameters;
 // pub mod parameters_loader;
 // pub mod run;
-// pub mod settings;
+pub mod settings;
 
 // pub use self::input::*;
 // pub use self::parameters::*;
 // pub use self::parameters_loader::*;
 // pub use self::run::*;
-// pub use self::settings::*;
+pub use self::settings::*;
