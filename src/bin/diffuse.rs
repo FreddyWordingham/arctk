9d299187b7020db4aa30af30cc03af9d9966402d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2021-01-23 21:41:37 +0000

    Added directory initialisation to diffuse binary.

--- src/bin/diffuse.rs
+++ src/bin/diffuse.rs
@@ -22,7 +22,7 @@ fn main() {
     let term_width = arctk::util::term::width().unwrap_or(80);
     title(term_width, "Diffuse");
 
-    // let (in_dir, out_dir, params_path) = initialisation(term_width);
+    let (in_dir, out_dir, params_path) = initialisation(term_width);
     // let params = load_parameters(term_width, &in_dir, &params_path);
 
     // section(term_width, "Input");
@@ -62,30 +62,30 @@ fn main() {
     section(term_width, "Finished");
 }
 
-// /// Initialise the input arguments.
-// fn initialisation(term_width: usize) -> (PathBuf, PathBuf, PathBuf) {
-//     section(term_width, "Initialisation");
-//     sub_section(term_width, "args");
-//     args!(
-//         bin_path: PathBuf;
-//         input_dir: PathBuf;
-//         output_dir: PathBuf;
-//         params_path: PathBuf
-//     );
-//     report!(bin_path.display(), "binary path");
-//     report!(input_dir.display(), "relative input path");
-//     report!(output_dir.display(), "relative output path");
-//     report!(params_path.display(), "parameters");
+/// Initialise the input arguments.
+fn initialisation(term_width: usize) -> (PathBuf, PathBuf, PathBuf) {
+    section(term_width, "Initialisation");
+    sub_section(term_width, "args");
+    args!(
+        bin_path: PathBuf;
+        input_dir: PathBuf;
+        output_dir: PathBuf;
+        params_path: PathBuf
+    );
+    report!(bin_path.display(), "binary path");
+    report!(input_dir.display(), "relative input path");
+    report!(output_dir.display(), "relative output path");
+    report!(params_path.display(), "parameters");
 
-//     sub_section(term_width, "directories");
-//     let cwd = current_dir().expect("Failed to determine current working directory.");
-//     let (in_dir, out_dir) = dir::io_dirs(Some(cwd.join(input_dir)), Some(cwd.join(output_dir)))
-//         .expect("Failed to initialise directories.");
-//     report!(in_dir.display(), "input directory");
-//     report!(out_dir.display(), "output directory");
+    sub_section(term_width, "directories");
+    let cwd = current_dir().expect("Failed to determine current working directory.");
+    let (in_dir, out_dir) = dir::io_dirs(Some(cwd.join(input_dir)), Some(cwd.join(output_dir)))
+        .expect("Failed to initialise directories.");
+    report!(in_dir.display(), "input directory");
+    report!(out_dir.display(), "output directory");
 
-//     (in_dir, out_dir, params_path)
-// }
+    (in_dir, out_dir, params_path)
+}
 
 // /// Load the required files and form the input parameters.
 // fn load_parameters(term_width: usize, in_dir: &Path, params_path: &Path) -> Parameters {
