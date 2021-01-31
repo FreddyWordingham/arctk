05381f52061cc3ca58e79e63af8f1916cc0596d6
Freddy Wordingham <fjmw201@exeter.ac.uk>
2021-01-31 01:59:10 +0000

    Added reactor settings.

--- src/sim/reactor/mod.rs
+++ src/sim/reactor/mod.rs
@@ -6,9 +6,10 @@
 // pub mod parameters_builder;
 // pub mod parameters_builder_loader;
 // pub mod run;
-// pub mod settings;
+pub mod settings;
 // pub mod stencil;
 
+pub use self::settings::*;
 // pub use self::{
 //     input::*, output::*, parameters::*, parameters_builder::*, parameters_builder_loader::*,
 //     run::*, settings::*, stencil::*,
---
+++ src/sim/reactor/settings.rs
@@ -0,0 +1,55 @@
+//! Reactor settings.
+
+use crate::{clone, fmt_report};
+use arctk_attr::file;
+use std::fmt::{Display, Error, Formatter};
+
+/// General settings structure.
+#[file]
+pub struct Settings {
+    /// Total integration time [s].
+    time: f64,
+    /// Number of intermediate dumps.
+    dumps: usize,
+    /// Quality parameter [0:1].
+    quality: f64,
+    /// Minimum timestep [s].
+    min_time: f64,
+}
+
+impl Settings {
+    clone!(time, f64);
+    clone!(dumps, usize);
+    clone!(quality, f64);
+    clone!(min_time, f64);
+
+    /// Construct a new instance.
+    #[inline]
+    #[must_use]
+    pub fn new(time: f64, dumps: usize, quality: f64, min_time: f64) -> Self {
+        debug_assert!(time > 0.0);
+        debug_assert!(dumps > 0);
+        debug_assert!(quality > 0.0);
+        debug_assert!(quality < 1.0);
+        debug_assert!(min_time < time);
+
+        Self {
+            time,
+            dumps,
+            quality,
+            min_time,
+        }
+    }
+}
+
+impl Display for Settings {
+    #[inline]
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+        writeln!(fmt, "...")?;
+        fmt_report!(fmt, self.time, "integration time (s)");
+        fmt_report!(fmt, self.dumps, "intermediate dumps");
+        fmt_report!(fmt, self.quality, "quality parameter");
+        fmt_report!(fmt, self.min_time, "minimum timestep (s)");
+        Ok(())
+    }
+}
