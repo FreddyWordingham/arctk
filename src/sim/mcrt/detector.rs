88a514567e43604fd18463092e72cc107890abed
Freddy Wordingham <fjmw201@exeter.ac.uk>
2021-02-03 20:00:48 +0000

    Implemented save trait for detector structure.

--- src/sim/mcrt/detector.rs
+++ src/sim/mcrt/detector.rs
@@ -1,7 +1,10 @@
 //! Optical detectors.
 
-use crate::{data::Histogram, fmt_report};
-use std::fmt::{Display, Error, Formatter};
+use crate::{data::Histogram, err::Error, fmt_report, fs::Save};
+use std::{
+    fmt::{Display, Formatter},
+    path::Path,
+};
 
 /// Detectors.
 pub enum Detector {
@@ -9,9 +12,22 @@ pub enum Detector {
     Spectrometer(Histogram),
 }
 
+impl Save for Detector {
+    #[inline]
+    fn save_data(&self, path: &Path) -> Result<(), Error> {
+        match *self {
+            Self::Spectrometer(ref hist) => {
+                hist.save(&path.with_extension("csv"))?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
 impl Display for Detector {
     #[inline]
-    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), std::fmt::Error> {
         match *self {
             Self::Spectrometer(ref hist) => {
                 fmt_report!(fmt, "Spectrometer", "kind");
