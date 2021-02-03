a514b0673a5c347f1422fe2d46dcff835d483933
Freddy Wordingham <fjmw201@exeter.ac.uk>
2021-02-03 22:21:19 +0000

    Added attribute spectrometer linker stage.

---
+++ src/sim/mcrt/attribute_linker_linker.rs
@@ -0,0 +1,61 @@
+//! Attribute first-stage linker.
+
+use crate::{
+    err::Error,
+    ord::{Link, Name, Set},
+    sim::mcrt::AttributeLinker,
+};
+use arctk_attr::file;
+use std::fmt::{Display, Formatter};
+
+/// Surface attribute setup.
+#[file]
+pub enum AttributeLinkerLinker {
+    /// Material interface, inside material name, outside material name.
+    Interface(Name, Name),
+    /// Partially reflective mirror, reflection fraction.
+    Mirror(f64),
+    /// Spectrometer id.
+    Spectrometer(Name),
+}
+
+impl<'a> Link<'a, usize> for AttributeLinkerLinker {
+    type Inst = AttributeLinker;
+
+    #[inline]
+    fn requires(&self) -> Vec<Name> {
+        match *self {
+            Self::Interface(..) | Self::Mirror(..) => vec![],
+            Self::Spectrometer(ref id) => vec![id.clone()],
+        }
+    }
+
+    #[inline]
+    fn link(self, reg: &'a Set<usize>) -> Result<Self::Inst, Error> {
+        Ok(match self {
+            Self::Interface(inside, outside) => Self::Inst::Interface(inside, outside),
+            Self::Mirror(r) => Self::Inst::Mirror(r),
+            Self::Spectrometer(id) => Self::Inst::Spectrometer(
+                *reg.get(&id)
+                    .unwrap_or_else(|| panic!("Failed to link attribute-spectrometer key: {}", id)),
+            ),
+        })
+    }
+}
+
+impl Display for AttributeLinkerLinker {
+    #[inline]
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), std::fmt::Error> {
+        match *self {
+            Self::Interface(ref in_mat, ref out_mat) => {
+                write!(fmt, "Interface: {} :| {}", in_mat, out_mat)
+            }
+            Self::Mirror(abs) => {
+                write!(fmt, "Mirror: {}% abs", abs * 100.0)
+            }
+            Self::Spectrometer(ref id) => {
+                write!(fmt, "Spectrometer: {}", id)
+            }
+        }
+    }
+}
