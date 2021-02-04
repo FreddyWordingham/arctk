c8f97cda4eb13db9c26d22456dac2fb540dc5294
Freddy Wordingham <fjmw201@exeter.ac.uk>
2021-02-04 15:11:14 +0000

    Added peel_off function.

--- src/sim/mcrt/peel_off.rs
+++ src/sim/mcrt/peel_off.rs
@@ -1,11 +1,85 @@
 //! Peel-off function.
 
-use crate::{math::Pos3, phys::Photon};
+use crate::{
+    math::{Dir3, Pos3},
+    phys::{Local, Photon},
+    sim::mcrt::{Attribute, Input},
+};
+use nalgebra::distance;
+
+/// Minimum weight before discarding during peel-off.
+const THRESHOLD: f64 = 1.0e-6;
 
 /// Calculate the change in weight over a give flight towards a given point.
 /// Perform a peel off event.
 #[inline]
 #[must_use]
-pub fn peel_off(mut _phot: Photon, _pos: Pos3) -> Option<f64> {
-    unimplemented!();
+pub fn peel_off(input: &Input, mut phot: Photon, env: &Local, pos: Pos3) -> Option<f64> {
+    let g = env.asym();
+    let g_sq = g * g;
+
+    let dir = Dir3::new_normalize(pos - phot.ray().pos());
+
+    let cos_ang = phot.ray().dir().dot(&dir);
+    let mut prob = 0.5 * ((1.0 - g_sq) / (1.0 + g_sq - (2.0 * g * cos_ang)).powf(1.5));
+
+    if prob < THRESHOLD {
+        return None;
+    }
+
+    *phot.ray_mut().dir_mut() = dir;
+
+    let loop_limit = input.sett.loop_limit();
+    let bump_dist = input.sett.bump_dist();
+    let mut inter_coeff = env.inter_coeff();
+
+    // Main trace loop.
+    let mut num_loops = 0;
+    let mut tar_dist;
+    loop {
+        // Loop limit check.
+        if num_loops >= loop_limit {
+            println!("[WARN] : Terminating peel-off: loop limit reached.");
+            break;
+        }
+        num_loops += 1;
+
+        // Interaction distances.
+        tar_dist = distance(&pos, phot.ray().pos());
+        let surf_hit = input.tree.scan(phot.ray().clone(), bump_dist, tar_dist);
+
+        // Event handling.
+        if let Some(hit) = surf_hit {
+            // Move to the collision point.
+            let x = hit.dist();
+            phot.ray_mut().travel(x);
+            prob *= (-x * inter_coeff).exp();
+
+            // Do something at the collision point.
+            match *hit.tag() {
+                Attribute::Interface(ref inside, ref outside) => {
+                    // Determine far side material.
+                    inter_coeff = if hit.side().is_inside() {
+                        outside.sample_environment(phot.wavelength()).inter_coeff()
+                    } else {
+                        inside.sample_environment(phot.wavelength()).inter_coeff()
+                    };
+                }
+                Attribute::Mirror(..) | Attribute::Spectrometer(..) => return None,
+            }
+
+        // phot.ray_mut().travel(hit.dist() + bump_dist);
+        // prob *= (-hit.dist() * inter_coeff).exp();
+        // env
+        } else {
+            prob *= (-tar_dist * inter_coeff).exp();
+            return Some(prob);
+        }
+
+        if prob <= THRESHOLD {
+            return None;
+        }
+    }
+
+    Some(prob)
 }
