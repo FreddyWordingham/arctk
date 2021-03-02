a0347504ff0c58cff1e8f218a8ddfc373116b4bb
Freddy Wordingham <fjmw201@exeter.ac.uk>
2021-03-02 14:16:43 +0000

    Corrected diffusion process.

--- src/sim/diffuse/run.rs
+++ src/sim/diffuse/run.rs
@@ -4,13 +4,16 @@ use crate::{
     err::Error,
     fs::Save,
     math::Vec3,
-    ord::{X, Y},
     sim::diffuse::{stencil, Input},
-    tools::ProgressBar,
+    tools::{ProgressBar, SilentProgressBar},
 };
-use ndarray::Array3;
+use ndarray::{Array1, Array3};
 use ndarray_stats::QuantileExt;
-use std::path::PathBuf;
+use rayon::prelude::*;
+use std::{
+    path::PathBuf,
+    sync::{Arc, Mutex},
+};
 
 /// Run a single-threaded Diffuse simulation.
 /// # Errors
@@ -22,6 +25,7 @@ pub fn single_thread(
     mut values: Array3<f64>,
     out_dir: &PathBuf,
 ) -> Result<Array3<f64>, Error> {
+    // Constants.
     let voxel_size = input.grid.voxel_size();
     let voxel_size_sq = Vec3::new(
         voxel_size.x * voxel_size.x,
@@ -37,15 +41,24 @@ pub fn single_thread(
     let max_dt = min_voxel_size_sq / (8.0 * max_coeff);
     let dt = max_dt * (1.0 - input.sett.quality()).min(1.0).max(0.0);
 
-    let steps = input.sett.dumps();
+    let steps = 1 + input.sett.dumps();
     let step_time = input.sett.time() / steps as f64;
+
+    // Allocation.
     let mut rates = Array3::zeros(*input.grid.res());
+
+    // Initial value write.
+    values.save(&out_dir.join(&format!("{:03}_diff.nc", 0)))?;
+    rates.save(&out_dir.join(&format!("{:03}_rate.nc", 0)))?;
+
+    // Time loop.
     for n in 0..steps {
-        let vr = integrate(input, values, rates, &voxel_size_sq, step_time, dt);
+        let vr = diffuse(input, &voxel_size_sq, step_time, dt, values, rates);
         values = vr.0;
         rates = vr.1;
-        values.save(&out_dir.join(&format!("{:03}_diff.nc", n)))?;
-        rates.save(&out_dir.join(&format!("{:03}_rate.nc", n)))?;
+
+        values.save(&out_dir.join(&format!("{:03}_diff.nc", n + 1)))?;
+        rates.save(&out_dir.join(&format!("{:03}_rate.nc", n + 1)))?;
     }
 
     Ok(values)
@@ -57,57 +70,91 @@ pub fn single_thread(
 #[allow(clippy::expect_used)]
 #[inline]
 #[must_use]
-pub fn integrate(
+pub fn diffuse(
     input: &Input,
-    mut values: Array3<f64>,
-    mut rates: Array3<f64>,
     voxel_size_sq: &Vec3,
     time: f64,
-    tar_dt: f64,
+    mut dt: f64,
+    mut values: Array3<f64>,
+    rates: Array3<f64>,
 ) -> (Array3<f64>, Array3<f64>) {
     debug_assert!(time > 0.0);
-    debug_assert!(tar_dt > 0.0);
+    debug_assert!(dt > 0.0);
 
-    let num_steps = (time / tar_dt) as usize;
-    let dt = time / num_steps as f64;
+    // Constants.
+    let steps = 1 + (time / dt) as usize;
+    dt = time / steps as f64;
 
-    let mut pb = ProgressBar::new("Diffusing", num_steps);
-    for _ in 0..num_steps {
-        rates = calc_rates(input, &values, rates, voxel_size_sq);
-        values += &(&rates * dt);
+    // Threading.
+    let rates = Mutex::new(rates);
+    let threads: Vec<_> = (0..num_cpus::get()).collect();
 
+    // Evolution.
+    let mut pb = ProgressBar::new("Diffusing", steps);
+    for _n in 0..steps {
+        // Calculate diffusion rates.
+        let spb = Arc::new(Mutex::new(SilentProgressBar::new(values.len())));
+        let _out: Vec<_> = threads
+            .par_iter()
+            .map(|_id| calc_diffuse_rates(input, voxel_size_sq, &values, &rates, &Arc::clone(&spb)))
+            .collect();
+
+        // Apply diffusion.
+        values += &(&(*rates.lock().expect("Could not lock rates array.")) * dt);
+
+        // Apply source terms.
+        values += &(input.sources * dt);
+
+        // Check for zero.
         values.mapv_inplace(|x| x.max(0.0));
 
-        // Potentially check for -ve values here.
+        // Tick.
         pb.tick();
     }
-    pb.finish_with_message("Step complete.");
+
+    let rates = rates.into_inner().expect("Failed to unwrap rates array.");
 
     (values, rates)
 }
 
-/// Rate calculation function.
+/// Calculate the diffusion rates.
 #[allow(clippy::expect_used)]
 #[inline]
-#[must_use]
-fn calc_rates(
+fn calc_diffuse_rates(
     input: &Input,
-    values: &Array3<f64>,
-    mut rates: Array3<f64>,
     voxel_size_sq: &Vec3,
-) -> Array3<f64> {
+    values: &Array3<f64>,
+    rates: &Mutex<Array3<f64>>,
+    pb: &Arc<Mutex<SilentProgressBar>>,
+) {
+    // Constants.
     let res = *input.grid.res();
+    let block_size = input.sett.block_size();
 
-    for n in 0..values.len() {
-        let xi = n % res[X];
-        let yi = (n / res[X]) % res[Y];
-        let zi = n / (res[X] * res[Y]);
+    // Allocation.
+    let mut holder = Array1::zeros(block_size);
 
-        let index = [xi, yi, zi];
+    // Rate calculations.
+    while let Some((start, end)) = {
+        let mut pb = pb.lock().expect("Could not lock progress bar.");
+        let b = pb.block(block_size);
+        std::mem::drop(pb);
+        b
+    } {
+        // Calculate rates.
+        for n in start..end {
+            let index = crate::tools::index::linear_to_three_dim(n, &res);
+            let stencil = stencil::Grad::new(index, values);
+            holder[n - start] = stencil.rate(input.coeffs[index], voxel_size_sq);
+        }
 
-        let stencil = stencil::Grad::new(index, values);
-        rates[index] = stencil.rate(input.coeffs[index], voxel_size_sq) + input.sources[index];
+        // Store rates.
+        {
+            let mut rates = rates.lock().expect("Could not lock rate array.");
+            for n in start..end {
+                let index = crate::tools::index::linear_to_three_dim(n, &res);
+                rates[index] = holder[n - start];
+            }
+        }
     }
-
-    rates
 }
