610073feb2444e728e6c1e79f4040ed31aad4d3e
Freddy Wordingham <f.wordingham@exeter.ac.uk>
2021-04-09 18:38:59 +0100

    Added cell size parameter.

--- src/sim/powder/parameters.rs
+++ src/sim/powder/parameters.rs
@@ -10,6 +10,8 @@ use std::fmt::{Display, Error, Formatter};
 /// Runtime parameters.
 #[file]
 pub struct Parameters {
+    /// Cell size (pixels).
+    pub cell_size: usize,
     /// Simulation resolution.
     pub res: [usize; 2],
 }
@@ -18,10 +20,26 @@ impl Display for Parameters {
     #[inline]
     fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
         writeln!(fmt, "...")?;
+        fmt_report!(fmt, self.cell_size, "cell size (pixels)");
         fmt_report!(
             fmt,
-            &format!("[{} x {}]", self.res[X], self.res[Y]),
-            "resolution"
+            &format!(
+                "[{} x {}] = {}",
+                self.res[X],
+                self.res[Y],
+                self.res[X] * self.res[Y]
+            ),
+            "sim resolution"
+        );
+        fmt_report!(
+            fmt,
+            &format!(
+                "[{} x {}] = {}",
+                self.res[X] * self.cell_size,
+                self.res[Y] * self.cell_size,
+                self.res[X] * self.res[Y] * self.cell_size * self.cell_size
+            ),
+            "window resolution"
         );
         Ok(())
     }
