632fa5c9097d0c1d7a8cda2b4237e7197e076f4e
Freddy Wordingham <f.wordingham@exeter.ac.uk>
2021-04-27 12:47:18 +0100

    Added track structure.

--- src/geom/shape/mod.rs
+++ src/geom/shape/mod.rs
@@ -4,6 +4,7 @@ pub mod cube;
 pub mod mesh;
 pub mod mesh_loader;
 pub mod smooth_triangle;
+pub mod track;
 pub mod triangle;
 
-pub use self::{cube::*, mesh::*, mesh_loader::*, smooth_triangle::*, triangle::*};
+pub use self::{cube::*, mesh::*, mesh_loader::*, smooth_triangle::*, track::*, triangle::*};
