22e50b4dfad9895f618c56c67e54fa6266b8fb3c
Freddy Wordingham <f.wordingham@exeter.ac.uk>
2021-05-03 19:58:58 +0100

    Added perlin generator.

--- script/arctk/__main__.py
+++ script/arctk/__main__.py
@@ -3,50 +3,64 @@ import matplotlib.image as img
 import numpy as np
 from progress.bar import Bar
 from perlin import Perlin
+import math
 
 
 def __main__():
     print("Hello arctk!")
 
-    powers = [5, 10, 20, 4, 234, 68, 3, 8, 6, 9, 192, 93]
+    powers = [10, 20, 4, 234, 68, 9, 192, 93]
     noise = []
     ratios = []
     for p in powers:
         noise.append(Perlin(p, p))
         ratios.append(1.0 / p)
 
-    nx = 100
-    ny = 100
+    l = 7
+    p = 8
+    nx = 2 ** p
+    ny = 2 ** p
     fx = 1.0 / nx
     fy = 1.0 / ny
 
     samples = np.zeros([nx, ny])
-    bar = Bar("Sampling", max=nx * ny)
-    for iy in range(ny):
-        y = iy * fy
-        for ix in range(nx):
-            x = ix * fx
-            samples[ix, iy] = sample(noise, ratios, x, y)
-            bar.next()
-    bar.finish()
+    dt = 0.1
+    for t in range(1000):
+        bar = Bar("Sampling", max=nx * ny)
+        for iy in range(ny):
+            y = iy * fy
+            for ix in range(nx):
+                x = ix * fx
+                samples[ix, iy] = sample(noise, ratios, x, y)
+                bar.next()
+        bar.finish()
 
-    min = samples.min()
-    max = samples.max()
-    samples = (samples - min) / (max - min)
-    samples = np.vectorize(clamp)(samples)
-    samples *= 256
+        min = samples.min()
+        max = samples.max()
+        samples = (samples - min) / (max - min)
 
-    fig = plt.figure(figsize=(6, 3.2))
+        for iy in range(ny):
+            for ix in range(nx):
+                x = samples[ix, iy]
+                x = math.floor(x * l) / l
+                samples[ix, iy] = x
 
-    ax = fig.add_subplot(111)
-    plt.imshow(samples)
-    ax.set_aspect('equal')
+        # samples = np.vectorize(step)(samples)
 
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-    ax.patch.set_alpha(0)
-    ax.set_frame_on(False)
-    plt.savefig("noise.png", bbox_inches='tight')
+        fig = plt.figure(figsize=(6, 3.2))
+
+        ax = fig.add_subplot(111)
+        plt.imshow(samples)
+        ax.set_aspect('equal')
+
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+        ax.patch.set_alpha(0)
+        ax.set_frame_on(False)
+        plt.savefig(f"output/noise_{t:03}.png", bbox_inches='tight')
+
+        for n in noise:
+            n.update(dt)
 
 
 def sample(noise, ratios, x, y):
@@ -56,14 +70,8 @@ def sample(noise, ratios, x, y):
     return t
 
 
-def clamp(x):
-    if x < 0.25:
-        return 0.25
-
-    if x > 0.75:
-        return 0.75
-
-    return x
+def step(x):
+    2.0 * x
 
 
 __main__()
--- script/arctk/perlin copy.py
+++ script/arctk/perlin copy.py
@@ -5,33 +5,18 @@ import numpy as np
 
 class Perlin:
     grid = []
-    rates = []
 
     def __init__(self, nx, ny):
         assert(nx >= 2)
         assert(ny >= 2)
 
         self.grid = np.empty((nx, ny, 2))
-        self.rates = np.empty((nx, ny))
 
         for iy in range(ny):
             for ix in range(nx):
                 theta = random.uniform(0.0, 2.0 * math.pi)
                 self.grid[ix, iy, 0] = math.sin(theta)
                 self.grid[ix, iy, 1] = math.cos(theta)
-                self.rates[ix, iy] = random.uniform(0.0, 1.0)
-
-    def update(self, t):
-        (nx, ny, _) = self.grid.shape
-        for iy in range(ny):
-            for ix in range(nx):
-                x = self.grid[ix, iy, 0]
-                y = self.grid[ix, iy, 1]
-                r = self.rates[ix, iy]
-                self.grid[ix, iy, 0] = (
-                    x * math.cos(t * r)) - (y * math.sin(t * r))
-                self.grid[ix, iy, 1] = (
-                    x * math.sin(t * r)) + (y * math.cos(t * r))
 
     def sample(self, x, y):
         assert(x >= 0)
