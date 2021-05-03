f3d8e4c410e9ffd1136dced3a510a30785d9d842
Freddy Wordingham <f.wordingham@exeter.ac.uk>
2021-05-03 12:11:39 +0100

    Added python library files.

--- scripts/arctk/__init__.py
+++ scripts/arctk/__init__.py
@@ -1,2 +1,11 @@
-def hello_arctk():
-    print("Hello arctk!")
+import vectormath as vmath
+import math
+import random
+
+# V_RES = 240
+# H_RES = 480
+
+print("Hello arctk!")
+
+
+def 
---
+++ scripts/arctk/__main__.py
@@ -0,0 +1,15 @@
+import vectormath as vmath
+import math
+import random
+
+
+def hello_arctk():
+    print("Hello arctk!")
+
+
+def perlin(nx, ny):
+    theta = random.uniform(0.0, 2.0 * math.pi)
+
+
+def __main__():
+    print("Hello arctk!")
