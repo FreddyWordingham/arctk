c97662303f95e1f4990b4c99fe50f873e183f040
Freddy Wordingham <f.wordingham@exeter.ac.uk>
2021-05-20 15:48:50 +0100

    Updated cartographer shape test parameterisation.

--- input/test/all.sh
+++ input/test/all.sh
@@ -1,20 +1,23 @@
-read -rsp $'Press any key to continue...\n' -n1 key
-mcrt output input/ test/mcrt/absorption.json5;
+# read -rsp $'Press any key to continue...\n' -n1 key;
+# mcrt output input/ test/mcrt/absorption.json5;
 
-read -rsp $'Press any key to continue...\n' -n1 key
-mcrt output input/ test/mcrt/ccd.json5;
+# read -rsp $'Press any key to continue...\n' -n1 key;
+# mcrt output input/ test/mcrt/ccd.json5;
 
-read -rsp $'Press any key to continue...\n' -n1 key
-mcrt output input/ test/mcrt/imager.json5;
+# read -rsp $'Press any key to continue...\n' -n1 key;
+# mcrt output input/ test/mcrt/imager.json5;
 
-read -rsp $'Press any key to continue...\n' -n1 key
-mcrt output input/ test/mcrt/photo.json5;
+# read -rsp $'Press any key to continue...\n' -n1 key;
+# mcrt output input/ test/mcrt/photo.json5;
 
-read -rsp $'Press any key to continue...\n' -n1 key
-mcrt output input/ test/mcrt/spectrometer.json5;
+# read -rsp $'Press any key to continue...\n' -n1 key;
+# mcrt output input/ test/mcrt/spectrometer.json5;
 
-read -rsp $'Press any key to continue...\n' -n1 key
+read -rsp $'Press any key to continue...\n' -n1 key;
 babbage output input/ test/babbage/gen_nano_map.json5;
 
-read -rsp $'Press any key to continue...\n' -n1 key
+read -rsp $'Press any key to continue...\n' -n1 key;
 babbage output input/ test/babbage/gen_point_map.json5
+
+read -rsp $'Press any key to continue...\n' -n1 key;
+cartographer output input/ test/cartographer/shape.json5
