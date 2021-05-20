c09a2636f8482574b436066cee6eb8284afaaea0
Freddy Wordingham <f.wordingham@exeter.ac.uk>
2021-05-21 00:32:05 +0100

    Added diffuse point test parameterisation.

--- input/test/all.sh
+++ input/test/all.sh
@@ -1,3 +1,17 @@
+read -rsp $'Press any key to continue...\n' -n1 key;
+babbage output input/ test/babbage/gen_nano_map.json5;
+mv output/nano.nc input/res/maps/;
+
+read -rsp $'Press any key to continue...\n' -n1 key;
+babbage output input/ test/babbage/gen_point_map.json5;
+mv output/point_50.nc input/res/maps/;
+
+read -rsp $'Press any key to continue...\n' -n1 key;
+cartographer output input/ test/cartographer/shape.json5
+
+read -rsp $'Press any key to continue...\n' -n1 key;
+diffuse output input/ test/diffuse/point.json5;
+
 # read -rsp $'Press any key to continue...\n' -n1 key;
 # mcrt output input/ test/mcrt/absorption.json5;
 
@@ -12,12 +26,3 @@
 
 # read -rsp $'Press any key to continue...\n' -n1 key;
 # mcrt output input/ test/mcrt/spectrometer.json5;
-
-read -rsp $'Press any key to continue...\n' -n1 key;
-babbage output input/ test/babbage/gen_nano_map.json5;
-
-read -rsp $'Press any key to continue...\n' -n1 key;
-babbage output input/ test/babbage/gen_point_map.json5
-
-read -rsp $'Press any key to continue...\n' -n1 key;
-cartographer output input/ test/cartographer/shape.json5
