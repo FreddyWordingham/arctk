58d57d23e980c618a0e61681c1a170bf60fbd117
Freddy Wordingham <f.wordingham@exeter.ac.uk>
2021-05-22 19:35:43 +0100

    Added reactor diffusion test.

--- input/test/test.sh
+++ input/test/test.sh
@@ -19,30 +19,36 @@
 # mv output/cube_100.nc input/res/maps/;
 # diffuse output/test/diffuse/cube input/ test/diffuse/cube.json5;
 
-read -rsp $'Flask Test: simple reaction\nPress any key to continue...' -n1 key;
-cargo run --bin flask --features="sim" output/test/flask/simple input/ test/flask/simple.json5;
-# flask output/test/flask/simple input/ test/flask/simple.json5;
-python3 input/plot.py output/test/flask/simple/values.csv
+# read -rsp $'Flask Test: simple reaction\nPress any key to continue...' -n1 key;
+# cargo run --bin flask --features="sim" output/test/flask/simple input/ test/flask/simple.json5;
+# # flask output/test/flask/simple input/ test/flask/simple.json5;
+# python3 input/plot.py output/test/flask/simple/values.csv
 
-read -rsp $'Flask Test: chain reaction\nPress any key to continue...' -n1 key;
-cargo run --bin flask --features="sim" output/test/flask/chain input/ test/flask/chain.json5;
-# flask output/test/flask/chain input/ test/flask/chain.json5;
-python3 input/plot.py output/test/flask/chain/values.csv
+# read -rsp $'Flask Test: chain reaction\nPress any key to continue...' -n1 key;
+# cargo run --bin flask --features="sim" output/test/flask/chain input/ test/flask/chain.json5;
+# # flask output/test/flask/chain input/ test/flask/chain.json5;
+# python3 input/plot.py output/test/flask/chain/values.csv
 
-read -rsp $'Flask Test: reversible reaction\nPress any key to continue...' -n1 key;
-cargo run --bin flask --features="sim" output/test/flask/equil input/ test/flask/equil.json5;
-# flask output/test/flask/equil input/ test/flask/equil.json5;
-python3 input/plot.py output/test/flask/equil/values.csv
+# read -rsp $'Flask Test: reversible reaction\nPress any key to continue...' -n1 key;
+# cargo run --bin flask --features="sim" output/test/flask/equil input/ test/flask/equil.json5;
+# # flask output/test/flask/equil input/ test/flask/equil.json5;
+# python3 input/plot.py output/test/flask/equil/values.csv
 
-read -rsp $'Flask Test: source term\nPress any key to continue...' -n1 key;
-cargo run --bin flask --features="sim" output/test/flask/source input/ test/flask/source.json5;
-# flask output/test/flask/source input/ test/flask/source.json5;
-python3 input/plot.py output/test/flask/source/values.csv
+# read -rsp $'Flask Test: source term\nPress any key to continue...' -n1 key;
+# cargo run --bin flask --features="sim" output/test/flask/source input/ test/flask/source.json5;
+# # flask output/test/flask/source input/ test/flask/source.json5;
+# python3 input/plot.py output/test/flask/source/values.csv
 
-read -rsp $'Flask Test: sink term\nPress any key to continue...' -n1 key;
-cargo run --bin flask --features="sim" output/test/flask/sink input/ test/flask/sink.json5;
-# flask output/test/flask/sink input/ test/flask/sink.json5;
-python3 input/plot.py output/test/flask/sink/values.csv
+# read -rsp $'Flask Test: sink term\nPress any key to continue...' -n1 key;
+# cargo run --bin flask --features="sim" output/test/flask/sink input/ test/flask/sink.json5;
+# # flask output/test/flask/sink input/ test/flask/sink.json5;
+# python3 input/plot.py output/test/flask/sink/values.csv
+
+read -rsp $'Reactor Test: diffusion\nPress any key to continue...' -n1 key;
+babbage output/tmp input/ test/babbage/gen_unit_101_map.json5;
+mv output/tmp/unit_101.nc input/res/maps/;
+reactor output/test/reactor/diffuse input/ test/reactor/diffuse.json5
+
 
 # read -rsp $'Press any key to continue...\n' -n1 key;
 # mcrt output input/ test/mcrt/absorption.json5;
