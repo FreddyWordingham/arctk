fe56f3ea484ded739f4aad338b09641657a521d0
Freddy Wordingham <f.wordingham@exeter.ac.uk>
2021-05-22 17:49:47 +0100

    Improved test script.

--- input/test/all.sh
+++ input/test/all.sh
@@ -1,23 +1,26 @@
-read -rsp $'Press any key to continue...\n' -n1 key;
-babbage output input/ test/babbage/gen_nano_map.json5;
-mv output/nano.nc input/res/maps/;
+read -rsp $'Babbage Test: generate filled map\nPress any key to continue...\n' -n1 key;
+babbage output/tmp input/ test/babbage/gen_nano_map.json5;
+mv output/tmp/nano.nc input/res/maps/;
 
-read -rsp $'Press any key to continue...\n' -n1 key;
-babbage output input/ test/babbage/gen_point_map.json5;
-mv output/point_50.nc input/res/maps/;
+read -rsp $'Babbage Test: generate point map\nPress any key to continue...\n' -n1 key;
+babbage output/tmp input/ test/babbage/gen_point_map.json5;
+mv output/tmp/point_50.nc input/res/maps/;
 
-read -rsp $'Press any key to continue...\n' -n1 key;
-cartographer output input/ test/cartographer/shape.json5
+# read -rsp $'Press any key to continue...\n' -n1 key;
+# cartographer output input/ test/cartographer/shape.json5
 
-read -rsp $'Press any key to continue...\n' -n1 key;
-diffuse output input/ test/diffuse/point.json5;
+# read -rsp $'Press any key to continue...\n' -n1 key;
+# diffuse output input/ test/diffuse/point.json5;
 
-read -rsp $'Press any key to continue...\n' -n1 key;
-babbage output input/ test/babbage/gen_tenth_map.json5;
-mv output/tenth.nc input/res/maps/;
-babbage output input/ test/babbage/gen_cube_map.json5;
-mv output/cube_100.nc input/res/maps/;
-diffuse output input/ test/diffuse/cube.json5;
+# read -rsp $'Press any key to continue...\n' -n1 key;
+# babbage output input/ test/babbage/gen_tenth_map.json5;
+# mv output/tenth.nc input/res/maps/;
+# babbage output input/ test/babbage/gen_cube_map.json5;
+# mv output/cube_100.nc input/res/maps/;
+# diffuse output input/ test/diffuse/cube.json5;
+
+# read -rsp $'Press any key to continue...\n' -n1 key;
+# flask output input/ test/flask/chain.json5;
 
 # read -rsp $'Press any key to continue...\n' -n1 key;
 # mcrt output input/ test/mcrt/absorption.json5;
