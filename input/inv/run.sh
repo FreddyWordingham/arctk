4c5a460faf8686b71abb09214fa1cfe8c26e3856
Freddy Wordingham <f.wordingham@exeter.ac.uk>
2021-05-29 20:29:07 +0100

    Added initial iron placement to haem reactor scripts.

--- input/inv/run.sh
+++ input/inv/run.sh
@@ -20,6 +20,7 @@ babbage output/tmp input/ inv/babbage/build_map_diff_ala.json5;
 mv output/tmp/diff_ala.nc input/res/maps/;
 
 
+
 #   Shallow tumour
 # read -rsp $'Shallow tumour: PpIX profile\nPress any key to continue...' -n1 key;
 touch 3a_Setup__shallow_cartography.txt;
@@ -41,3 +42,51 @@ touch 3d_Sim__photodynamic_therapy.txt;
 reactor output/inv/reactor/pdt/shallow input/ inv/reactor/pdt/shallow.json5;
 babbage output/tmp input/ inv/babbage/build_map_tumour_shallow_kill.json5;
 touch 3e_COMPLETE.txt;
+
+
+
+#   Thick tumour
+# read -rsp $'Thick tumour: PpIX profile\nPress any key to continue...' -n1 key;
+touch 4a_Setup__thick_cartography.txt;
+cartographer output/inv/cartographer/tumour/thick input/ inv/cartographer/tumour/thick.json5;
+babbage output/tmp input/ inv/babbage/build_map_multipliers_tumour_thick.json5;
+mv output/tmp/multipliers_thick.nc input/res/maps/;
+mv output/inv/cartographer/tumour/thick/map_\{tumour\}.nc input/res/maps/tumour/thick.nc;
+touch 4b_Setup__PPIX_generation.txt;
+reactor output/inv/reactor/ppix/thick input/ inv/reactor/ppix/thick.json5;
+mv output/inv/reactor/ppix/thick/099_\{ppix\}_diff.nc input/res/maps/init_ppix_thick.nc;
+mv output/inv/reactor/ppix/thick/099_\{ala\}_diff.nc input/res/maps/init_ala_thick.nc;
+
+#   PDT phase.
+# read -rsp $'Thick tumour: PDT phase\nPress any key to continue...' -n1 key;
+touch 4c_Sim__illumination.txt;
+mcrt output/inv/mcrt/thick input/ inv/mcrt/tumour/thick.json5;
+mv output/inv/mcrt/thick/shift_density.nc input/res/maps/udens_thick.nc;
+touch 4d_Sim__photodynamic_therapy.txt;
+reactor output/inv/reactor/pdt/thick input/ inv/reactor/pdt/thick.json5;
+babbage output/tmp input/ inv/babbage/build_map_tumour_thick_kill.json5;
+touch 4e_COMPLETE.txt;
+
+
+
+#   Deep tumour
+# read -rsp $'Deep tumour: PpIX profile\nPress any key to continue...' -n1 key;
+touch 5a_Setup__deep_cartography.txt;
+cartographer output/inv/cartographer/tumour/deep input/ inv/cartographer/tumour/deep.json5;
+babbage output/tmp input/ inv/babbage/build_map_multipliers_tumour_deep.json5;
+mv output/tmp/multipliers_deep.nc input/res/maps/;
+mv output/inv/cartographer/tumour/deep/map_\{tumour\}.nc input/res/maps/tumour/deep.nc;
+touch 5b_Setup__PPIX_generation.txt;
+reactor output/inv/reactor/ppix/deep input/ inv/reactor/ppix/deep.json5;
+mv output/inv/reactor/ppix/deep/099_\{ppix\}_diff.nc input/res/maps/init_ppix_deep.nc;
+mv output/inv/reactor/ppix/deep/099_\{ala\}_diff.nc input/res/maps/init_ala_deep.nc;
+
+#   PDT phase.
+# read -rsp $'Deep tumour: PDT phase\nPress any key to continue...' -n1 key;
+touch 5c_Sim__illumination.txt;
+mcrt output/inv/mcrt/deep input/ inv/mcrt/tumour/deep.json5;
+mv output/inv/mcrt/deep/shift_density.nc input/res/maps/udens_deep.nc;
+touch 5d_Sim__photodynamic_therapy.txt;
+reactor output/inv/reactor/pdt/deep input/ inv/reactor/pdt/deep.json5;
+babbage output/tmp input/ inv/babbage/build_map_tumour_deep_kill.json5;
+touch 5e_COMPLETE.txt;
--- input/inv/run_chelt.sh
+++ input/inv/run_chelt.sh
@@ -2,6 +2,8 @@
 # read -rsp $'Setup: Chelating agent diffusion\nPress any key to continue...' -n1 key;
 touch 4__Setup__chelt_cartography.txt;
 cartographer output/inv/cartographer/chelt input/ inv/cartographer/chelt.json5;
+babbage output/tmp input/ inv/babbage/build_map_init_fe.json5;
+mv output/tmp/init_fe.nc input/res/maps/;
 babbage output/tmp input/ inv/babbage/build_map_init_chelt.json5;
 mv output/tmp/init_chelt.nc input/res/maps/;
 babbage output/tmp input/ inv/babbage/build_map_diff_chelt.json5;
@@ -9,23 +11,21 @@ mv output/tmp/diff_chelt.nc input/res/maps/;
 
 
 #   Shallow tumour
-# read -rsp $'Shallow tumour: PpIX profile\nPress any key to continue...' -n1 key;
-touch 3a_Setup__shallow_cartography.txt;
-cartographer output/inv/cartographer/tumour/shallow input/ inv/cartographer/tumour/shallow.json5;
-babbage output/tmp input/ inv/babbage/build_map_multipliers_tumour_shallow.json5;
-mv output/tmp/multipliers_shallow.nc input/res/maps/;
-mv output/inv/cartographer/tumour/shallow/map_\{tumour\}.nc input/res/maps/tumour/shallow.nc;
-touch 3b_Setup__PPIX_generation.txt;
-reactor output/inv/reactor/ppix/shallow input/ inv/reactor/ppix/shallow.json5;
-mv output/inv/reactor/ppix/shallow/099_\{ppix\}_diff.nc input/res/maps/init_ppix_shallow.nc;
-mv output/inv/reactor/ppix/shallow/099_\{ala\}_diff.nc input/res/maps/init_ala_shallow.nc;
+# read -rsp $'Shallow tumour: Chelated PpIX profile\nPress any key to continue...' -n1 key;
+touch 5a_Setup__Chelt_PPIX_generation.txt;
+reactor output/inv/reactor/haem/shallow input/ inv/reactor/haem/shallow.json5;
+# mv output/inv/reactor/haem/shallow/099_\{ppix\}_diff.nc input/res/maps/init_ppix_shallow.nc;
+# mv output/inv/reactor/haem/shallow/099_\{ala\}_diff.nc input/res/maps/init_ala_shallow.nc;
+# mv output/inv/reactor/haem/shallow/099_\{fe\}_diff.nc input/res/maps/init_fe_shallow.nc;
+# mv output/inv/reactor/haem/shallow/099_\{chelt\}_diff.nc input/res/maps/init_haem_shallow.nc;
+# mv output/inv/reactor/haem/shallow/099_\{haem\}_diff.nc input/res/maps/init_haem_shallow.nc;
 
-#   PDT phase.
-# read -rsp $'Shallow tumour: PDT phase\nPress any key to continue...' -n1 key;
-touch 3c_Sim__illumination.txt;
-mcrt output/inv/mcrt/shallow input/ inv/mcrt/tumour/shallow.json5;
-mv output/inv/mcrt/shallow/shift_density.nc input/res/maps/udens_shallow.nc;
-touch 3d_Sim__photodynamic_therapy.txt;
-reactor output/inv/reactor/pdt/shallow input/ inv/reactor/pdt/shallow.json5;
-babbage output/tmp input/ inv/babbage/build_map_tumour_shallow_kill.json5;
-touch 3e_COMPLETE.txt;
+# #   PDT phase.
+# # read -rsp $'Shallow tumour: PDT phase\nPress any key to continue...' -n1 key;
+# touch 3c_Sim__illumination.txt;
+# mcrt output/inv/mcrt/shallow input/ inv/mcrt/tumour/shallow.json5;
+# mv output/inv/mcrt/shallow/shift_density.nc input/res/maps/udens_shallow.nc;
+# touch 3d_Sim__photodynamic_therapy.txt;
+# reactor output/inv/reactor/pdt/shallow input/ inv/reactor/pdt/shallow.json5;
+# babbage output/tmp input/ inv/babbage/build_map_tumour_shallow_kill.json5;
+# touch 3e_COMPLETE.txt;
