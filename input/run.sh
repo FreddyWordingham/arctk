9761032b5603486081320c432463d40fdac46c59
Freddy Wordingham <f.wordingham@exeter.ac.uk>
2021-06-01 00:47:30 +0100

    Added chapters to run script.

--- input/run.sh
+++ input/run.sh
@@ -2,5 +2,9 @@
 sh ./input/ch3/run.sh;
 # Fiducial model
 sh ./input/ch4/run.sh;
+# PTT I investigation
+sh ./input/ch5/run.sh;
+# PTT II investigation
+sh ./input/ch6/run.sh;
 # PDT investigation
 sh ./input/ch7/run.sh;
