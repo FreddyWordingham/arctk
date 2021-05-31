0726dfd818ea7333b69f123dd3e41d043da8c40f
Freddy Wordingham <f.wordingham@exeter.ac.uk>
2021-06-01 00:33:40 +0100

    Added chelation agen parameterisation.

--- input/ch7/run.sh
+++ input/ch7/run.sh
@@ -24,35 +24,19 @@ touch output/ch7/4c_COMPLETE.txt;
 
 #   Chelt setup.
 # read -rsp $'Setup: Chelating agent diffusion\nPress any key to continue...' -n1 key;
-touch 4__Setup__chelt_cartography.txt;
-cartographer output/inv/cartographer/chelt input/ inv/cartographer/chelt.json5;
-babbage output/tmp input/ inv/babbage/build_map_init_fe.json5;
+touch output/ch7/5a_Setup__chelt_cartography.txt;
+cartographer output/ch7/cartographer/chelt input/ ch7/cartographer/chelt.json5;
+babbage output/tmp input/ ch7/babbage/build_map_init_fe.json5;
 mv output/tmp/init_fe.nc input/res/maps/;
-babbage output/tmp input/ inv/babbage/build_map_init_chelt.json5;
+babbage output/tmp input/ ch7/babbage/build_map_init_chelt.json5;
 mv output/tmp/init_chelt.nc input/res/maps/;
-babbage output/tmp input/ inv/babbage/build_map_diff_chelt.json5;
+babbage output/tmp input/ ch7/babbage/build_map_diff_chelt.json5;
 mv output/tmp/diff_chelt.nc input/res/maps/;
-
-
-
-
-
-#   Shallow tumour
-# read -rsp $'Shallow tumour: Chelated PpIX profile\nPress any key to continue...' -n1 key;
-touch 5a_Setup__Chelt_PPIX_generation.txt;
-reactor output/inv/reactor/haem/shallow input/ inv/reactor/haem/shallow.json5;
-mv output/inv/reactor/haem/shallow/099_\{ppix\}_diff.nc input/res/maps/init_ppix_shallow.nc;
-mv output/inv/reactor/haem/shallow/099_\{ala\}_diff.nc input/res/maps/init_ala_shallow.nc;
-mv output/inv/reactor/haem/shallow/099_\{fe\}_diff.nc input/res/maps/init_fe_shallow.nc;
-mv output/inv/reactor/haem/shallow/099_\{chelt\}_diff.nc input/res/maps/init_haem_shallow.nc;
-mv output/inv/reactor/haem/shallow/099_\{haem\}_diff.nc input/res/maps/init_haem_shallow.nc;
-
-# #   PDT phase.
-# # read -rsp $'Shallow tumour: PDT phase\nPress any key to continue...' -n1 key;
-# touch 3c_Sim__illumination.txt;
-# mcrt output/inv/mcrt/shallow input/ inv/mcrt/tumour/shallow.json5;
-# mv output/inv/mcrt/shallow/shift_density.nc input/res/maps/udens_shallow.nc;
-# touch 3d_Sim__photodynamic_therapy.txt;
-# reactor output/inv/reactor/pdt/shallow input/ inv/reactor/pdt/shallow.json5;
-# babbage output/tmp input/ inv/babbage/build_map_tumour_shallow_kill.json5;
-# touch 3e_COMPLETE.txt;
+touch output/ch7/5b_Chelation.txt;
+reactor output/ch7/reactor/ppix/haem input/ ch7/reactor/ppix/haem.json5;
+mv output/ch7/reactor/ppix/haem/099_\{ala\}_diff.nc input/res/maps/init_ala_chelt.nc;
+mv output/ch7/reactor/ppix/haem/099_\{ppix\}_diff.nc input/res/maps/init_ppix_chelt.nc;
+mv output/ch7/reactor/ppix/haem/099_\{chelt\}_diff.nc input/res/maps/init_chelt_chelt.nc;
+mv output/ch7/reactor/ppix/haem/099_\{fe\}_diff.nc input/res/maps/init_fe_chelt.nc;
+touch output/ch7/5c_Chelt_PDT.txt;
+reactor output/ch7/reactor/chelt input/ ch7/reactor/chelt.json5;
