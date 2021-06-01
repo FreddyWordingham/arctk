e656c45a875f0ff510540e099a4f574f4013a7e5
Freddy Wordingham <f.wordingham@exeter.ac.uk>
2021-06-01 19:52:52 +0100

    Added test script.

--- input/ch3/run.sh
+++ input/ch3/run.sh
@@ -2,57 +2,56 @@ echo "Ch3: Babbage test I"
 babbage output/tmp input/ ch3/babbage/gen_nano_map.json5;
 mv output/tmp/nano.nc input/res/maps/;
 
-echo "Ch4: Babbage test II"
+echo "Ch3: Babbage test II"
 babbage output/tmp input/ ch3/babbage/gen_point_map.json5;
 mv output/tmp/point_50.nc input/res/maps/;
 
-echo "Ch4: Cartographer test I"
+
+echo "Ch3: Cartographer test I"
 cartographer output/ch3/cartographer/shape input/ ch3/cartographer/shape.json5
 
-echo "Ch4: Diffuse test  I"
+
+echo "Ch3: Diffuse test I"
 diffuse output/ch3/diffuse/kernel input/ ch3/diffuse/kernel.json5;
 
-echo "Ch4: Diffuse test  II"
+echo "Ch3: Diffuse test II"
 babbage output/tmp input/ ch3/babbage/gen_tenth_map.json5;
 mv output/tmp/tenth.nc input/res/maps/;
 babbage output/tmp input/ ch3/babbage/gen_cube_100_map.json5;
 mv output/tmp/cube_100.nc input/res/maps/;
 diffuse output/ch3/diffuse/cube input/ ch3/diffuse/cube.json5;
 
-# read -rsp $'Flask Test: simple reaction\nPress any key to continue...' -n1 key;
-# cargo run --bin flask --features="sim" output/ch3/flask/simple input/ ch3/flask/simple.json5;
-# # flask output/ch3/flask/simple input/ ch3/flask/simple.json5;
+
+echo "Ch3: Flask test I"
+cargo run --bin flask --features="sim" output/ch3/flask/simple input/ ch3/flask/simple.json5;
 # python3 input/plot.py output/ch3/flask/simple/values.csv
 
-# read -rsp $'Flask Test: chain reaction\nPress any key to continue...' -n1 key;
-# cargo run --bin flask --features="sim" output/ch3/flask/chain input/ ch3/flask/chain.json5;
-# # flask output/ch3/flask/chain input/ ch3/flask/chain.json5;
+echo "Ch3: Flask test II"
+cargo run --bin flask --features="sim" output/ch3/flask/chain input/ ch3/flask/chain.json5;
 # python3 input/plot.py output/ch3/flask/chain/values.csv
 
-# read -rsp $'Flask Test: reversible reaction\nPress any key to continue...' -n1 key;
-# cargo run --bin flask --features="sim" output/ch3/flask/equil input/ ch3/flask/equil.json5;
-# # flask output/ch3/flask/equil input/ ch3/flask/equil.json5;
-# python3 input/plot.py output/ch3/flask/equil/values.csv
+echo "Ch3: Flask test III"
+cargo run --bin flask --features="sim" output/ch3/flask/equilibrium input/ ch3/flask/equilibrium.json5;
+# python3 input/plot.py output/ch3/flask/chain/values.csv
 
-# read -rsp $'Flask Test: source term\nPress any key to continue...' -n1 key;
-# cargo run --bin flask --features="sim" output/ch3/flask/source input/ ch3/flask/source.json5;
-# # flask output/ch3/flask/source input/ ch3/flask/source.json5;
+echo "Ch3: Flask test IV"
+cargo run --bin flask --features="sim" output/ch3/flask/source input/ ch3/flask/source.json5;
 # python3 input/plot.py output/ch3/flask/source/values.csv
 
-# read -rsp $'Flask Test: sink term\nPress any key to continue...' -n1 key;
-# cargo run --bin flask --features="sim" output/ch3/flask/sink input/ ch3/flask/sink.json5;
-# # flask output/ch3/flask/sink input/ ch3/flask/sink.json5;
+echo "Ch3: Flask test V"
+cargo run --bin flask --features="sim" output/ch3/flask/sink input/ ch3/flask/sink.json5;
 # python3 input/plot.py output/ch3/flask/sink/values.csv
 
-# read -rsp $'Reactor Test: diffusion\nPress any key to continue...' -n1 key;
-babbage output/tmp input/ ch3/babbage/gen_unit_101_map.json5;
-mv output/tmp/unit_101.nc input/res/maps/;
-reactor output/ch3/reactor/diffuse input/ ch3/reactor/diffuse.json5
 
-# read -rsp $'Reactor Test: duel\nPress any key to continue...' -n1 key;
-babbage output/tmp input/ ch3/babbage/gen_cube_101_map.json5;
-mv output/tmp/cube_101.nc input/res/maps/;
-reactor output/ch3/reactor/duel input/ ch3/reactor/duel.json5
+echo "Ch3: Reactor test I"
+babbage output/tmp input/ ch3/babbage/gen_unit_51_map.json5;
+mv output/tmp/unit_51.nc input/res/maps/;
+reactor output/ch3/reactor/diffuse input/ ch3/reactor/diffuse.json5;
+
+echo "Ch3: Reactor test II"
+babbage output/tmp input/ ch3/babbage/gen_cube_51_map.json5;
+mv output/tmp/cube_51.nc input/res/maps/;
+reactor output/ch3/reactor/react input/ ch3/reactor/react.json5;
 
 
 # read -rsp $'Press any key to continue...\n' -n1 key;
