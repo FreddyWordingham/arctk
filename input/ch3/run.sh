49388ad66f90f1f23f10960f3d635931c2425514
Freddy Wordingham <f.wordingham@exeter.ac.uk>
2021-06-03 20:03:06 +0100

    Updated test simulation script.

--- input/ch3/run.sh
+++ input/ch3/run.sh
@@ -23,23 +23,23 @@ diffuse output/ch3/diffuse/cube input/ ch3/diffuse/cube.json5;
 
 
 echo "Ch3: Flask test I"
-cargo run --bin flask --features="sim" output/ch3/flask/simple input/ ch3/flask/simple.json5;
+flask output/ch3/flask/simple input/ ch3/flask/simple.json5;
 # python3 input/plot.py output/ch3/flask/simple/values.csv
 
 echo "Ch3: Flask test II"
-cargo run --bin flask --features="sim" output/ch3/flask/chain input/ ch3/flask/chain.json5;
+flask output/ch3/flask/chain input/ ch3/flask/chain.json5;
 # python3 input/plot.py output/ch3/flask/chain/values.csv
 
 echo "Ch3: Flask test III"
-cargo run --bin flask --features="sim" output/ch3/flask/equilibrium input/ ch3/flask/equilibrium.json5;
+flask output/ch3/flask/equilibrium input/ ch3/flask/equilibrium.json5;
 # python3 input/plot.py output/ch3/flask/chain/values.csv
 
 echo "Ch3: Flask test IV"
-cargo run --bin flask --features="sim" output/ch3/flask/source input/ ch3/flask/source.json5;
+flask output/ch3/flask/source input/ ch3/flask/source.json5;
 # python3 input/plot.py output/ch3/flask/source/values.csv
 
 echo "Ch3: Flask test V"
-cargo run --bin flask --features="sim" output/ch3/flask/sink input/ ch3/flask/sink.json5;
+flask output/ch3/flask/sink input/ ch3/flask/sink.json5;
 # python3 input/plot.py output/ch3/flask/sink/values.csv
 
 
@@ -55,27 +55,39 @@ babbage output/tmp input/ ch3/babbage/gen_cube_51_map.json5;
 mv output/tmp/cube_51.nc input/res/maps/;
 reactor output/ch3/reactor/react input/ ch3/reactor/react.json5;
 
+echo "Ch3: Reactor test III"
+reactor output/ch3/reactor/coupled input/ ch3/reactor/coupled.json5;
+
 
 echo "Ch3: MCRT test I"
-mcrt output/ch3/mcrt/absorption input/ ch3/mcrt/absorption.json5;
-
-echo "Ch3: MCRT test II"
-mcrt output/ch3/mcrt/ccd input/ ch3/mcrt/ccd.json5;
-
-echo "Ch3: MCRT test III"
-mcrt output/ch3/mcrt/rainbow input/ ch3/mcrt/rainbow.json5;
-
-echo "Ch3: MCRT test IV"
 mcrt output/ch3/mcrt/prism input/ ch3/mcrt/prism.json5;
 
+echo "Ch3: MCRT test II"
+mcrt output/ch3/mcrt/rainbow input/ ch3/mcrt/rainbow.json5;
+
+echo "Ch3: MCRT test III"
+mcrt output/ch3/mcrt/lens input/ ch3/mcrt/lens.json5;
+
+echo "Ch3: MCRT test IV"
+mcrt output/ch3/mcrt/fibre input/ ch3/mcrt/fibre.json5;
+
 echo "Ch3: MCRT test V"
+mcrt output/ch3/mcrt/absorption input/ ch3/mcrt/absorption.json5;
+
+echo "Ch3: MCRT test VI"
+mcrt output/ch3/mcrt/beam input/ ch3/mcrt/beam.json5;
+
+echo "Ch3: MCRT test VII"
+mcrt output/ch3/mcrt/scatter input/ ch3/mcrt/scatter.json5;
+
+echo "Ch3: MCRT test VIII"
+mcrt output/ch3/mcrt/ccd input/ ch3/mcrt/ccd.json5;
+
+echo "Ch3: MCRT test IX"
 mcrt output/ch3/mcrt/spectrometer input/ ch3/mcrt/spectrometer.json5;
 # python3 input/plot.py output/ch3/mcrt/spectrometer/spectrometer_\{spectrometer\}.csv
 
-echo "Ch3: MCRT test VI"
-mcrt output/ch3/mcrt/lens input/ ch3/mcrt/lens.json5;
-
-echo "Ch3: MCRT test VII"
+echo "Ch3: MCRT test X"
 mcrt output/ch3/mcrt/photo input/ ch3/mcrt/photo.json5;
 
 
