af7997115cfd349307fbc8538d5c8fc9b1fdf8a4
Freddy Wordingham <f.wordingham@exeter.ac.uk>
2021-06-03 21:19:47 +0100

    Added low res output.

--- input/ch6/run.sh
+++ input/ch6/run.sh
@@ -2,7 +2,7 @@ echo "Ch6: Spectrometer"
 mcrt output/ch6/mcrt/spectrometer input/ ch6/mcrt/spectrometer.json5;
 
 
-echo "Ch6: Spectrometer"
+echo "Ch6: Laser"
 mcrt output/ch6/mcrt/laser input/ ch6/mcrt/laser.json5;
 
 
@@ -29,8 +29,8 @@ mv output/tmp/init_chelt.nc input/res/maps/;
 babbage output/tmp input/ ch6/babbage/build_map_diff_chelt.json5;
 mv output/tmp/diff_chelt.nc input/res/maps/;
 reactor output/ch6/reactor/ppix/haem input/ ch6/reactor/ppix/haem.json5;
-mv output/ch6/reactor/ppix/haem/099_\{ala\}_diff.nc input/res/maps/init_ala_chelt.nc;
-mv output/ch6/reactor/ppix/haem/099_\{ppix\}_diff.nc input/res/maps/init_ppix_chelt.nc;
-mv output/ch6/reactor/ppix/haem/099_\{chelt\}_diff.nc input/res/maps/init_chelt_chelt.nc;
-mv output/ch6/reactor/ppix/haem/099_\{fe\}_diff.nc input/res/maps/init_fe_chelt.nc;
+mv output/ch6/reactor/ppix/haem/011_\{ala\}_diff.nc input/res/maps/init_ala_chelt.nc;
+mv output/ch6/reactor/ppix/haem/011_\{ppix\}_diff.nc input/res/maps/init_ppix_chelt.nc;
+mv output/ch6/reactor/ppix/haem/011_\{chelt\}_diff.nc input/res/maps/init_chelt_chelt.nc;
+mv output/ch6/reactor/ppix/haem/011_\{fe\}_diff.nc input/res/maps/init_fe_chelt.nc;
 reactor output/ch6/reactor/chelt input/ ch6/reactor/chelt.json5;
