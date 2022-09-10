4ccaf547b75843fbf833cf596293228977a365d2
FreddyWordingham <freddy@digilab.com>
2022-09-10 18:51:41 +0100

    Added documentation

--- src/bin/mcrt.rs
+++ src/bin/mcrt.rs
@@ -4,6 +4,10 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
+    /// Verbose mode
+    #[clap(short, long, value_parser)]
+    verbose: bool,
+
     /// Input directory
     #[clap(short, long, value_parser, default_value = ".")]
     input_dir: String,
@@ -12,19 +16,30 @@ struct Args {
     #[clap(short, long, value_parser, default_value = "./output")]
     output_dir: String,
 
-    /// Verbose mode
-    #[clap(short, long, value_parser)]
-    verbose: bool,
+    /// Parameters filename
+    #[clap(short, long, value_parser, default_value = "parameters.json")]
+    parameters_filename: String,
 }
 
+/// Entrypoint function.
 fn main() {
     let args = Args::parse();
-    init_directories(args.verbose, args.input_dir, args.output_dir);
+    load_parameters(args.verbose, args.input_dir, args.parameters_filename);
+    init_directories(args.verbose, args.output_dir);
 }
 
-fn init_directories(verbose: bool, input_dir: String, output_dir: String) {
+/// Load parameters from a JSON file.
+fn load_parameters(verbose: bool, input_dir: String, parameters_filename: String) {
+    let parameters_filepath = input_dir + "/" + &parameters_filename;
+
     if verbose {
-        println!(" input dir: {}", input_dir);
-        println!("output dir: {}", output_dir);
+        println!("Loading parameters file: {}", parameters_filepath);
+    }
+}
+
+/// Create the output directory if it does not exist.
+fn init_directories(verbose: bool, output_dir: String) {
+    if verbose {
+        println!("Creating output directory: {}", output_dir);
     }
 }
