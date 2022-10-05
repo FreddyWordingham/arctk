57c179b56ab396493632927f3a34dbfec53b6fc3
FreddyWordingham <freddy@digilab.com>
2022-10-05 19:35:36 +0100

    Removed diagnostic prints. Fixed typo

--- src/parse/csv.rs
+++ src/parse/csv.rs
@@ -19,9 +19,6 @@ pub fn load<T: Clone + Zero + FromStr>(path: &Path) -> Array2<T> {
 #[inline]
 #[must_use]
 pub fn read<T: Clone + Zero + FromStr>(s: &str) -> Array2<T> {
-    println!("Reading CSV file...");
-    println!("{}", s);
-
     let mut lines: Vec<_> = s.lines().filter(|line| !line.starts_with("//")).collect();
     let headings: Vec<_> = lines
         .remove(0)
@@ -30,7 +27,7 @@ pub fn read<T: Clone + Zero + FromStr>(s: &str) -> Array2<T> {
         .collect();
 
     let mut data = Array2::zeros((headings.len(), lines.len()));
-    for (i, line) in lines.into_iter().enumerate() {
+    for (j, line) in lines.into_iter().enumerate() {
         let row: Vec<_> = line
             .split(',')
             .map(str::parse)
@@ -38,10 +35,10 @@ pub fn read<T: Clone + Zero + FromStr>(s: &str) -> Array2<T> {
             .collect();
 
         if row.len() != headings.len() {
-            panic!("There must be the same number of columns in each row.\nThe header contains {} columnds, but line {} contains {} columns.", headings.len(), line, row.len());
+            panic!("There must be the same number of columns in each row.\nThe header contains {} columns, but line {} contains {} columns.", headings.len(), line, row.len());
         }
 
-        for (j, elem) in row.into_iter().enumerate() {
+        for (i, elem) in row.into_iter().enumerate() {
             data[(i, j)] = elem;
         }
     }
